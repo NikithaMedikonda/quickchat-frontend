7c2173f81597c57211832456aa739452fec5b300
nagusha <nagusha.madasu@everest.engineering>
2025-05-06 00:23:46 +0530

    Add get started button to welcome screen

--- src/screens/WelcomeScreen/WelcomeScreen.tsx
+++ src/screens/WelcomeScreen/WelcomeScreen.tsx
@@ -1,27 +1,30 @@
-import { View, StyleSheet, Image} from 'react-native'
+import { View, StyleSheet, Image, Button } from 'react-native'
 import React from 'react'
 
 const WelcomeScreen = () => {
-  return (
-    <View style={styles.container}>
-      <Image style={styles.image} source={require('../../assets/images/quickchat_logo.png')}
-      testID="logo-image"
-      />
-    </View>
-  )
+    return (
+        <View style={styles.container}>
+            <Image style={styles.image} source={require('../../assets/images/quickchat_logo.png')}
+                testID="logo-image"
+            />
+            <View>
+                <Button title="Get Started" />
+            </View>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
     container: {
-      alignItems: "center",
-      backgroundColor:"black",
-      flex:1
+        alignItems: "center",
+        backgroundColor: "black",
+        flex: 1
     },
     image: {
         position: "relative",
         width: 270,
         height: 270,
         top: 150
-      },
+    },
 })
 export default WelcomeScreen
