981b9da8f80805772a7036c67e67f5ca3b3ae9a8
nagusha <nagusha.madasu@everest.engineering>
2025-05-06 00:16:52 +0530

    Add styles container for layout styling

--- src/screens/WelcomeScreen/WelcomeScreen.tsx
+++ src/screens/WelcomeScreen/WelcomeScreen.tsx
@@ -1,12 +1,19 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 
 const WelcomeScreen = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>WelcomeScreen</Text>
     </View>
   )
 }
 
+const styles = StyleSheet.create({
+    container: {
+      alignItems: "center",
+      backgroundColor:"black",
+      flex:1
+    },
+})
 export default WelcomeScreen
