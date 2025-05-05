f8227ad9cb6a57e2e1a47e7c66767ed19ffe44c4
nagusha <nagusha.madasu@everest.engineering>
2025-05-06 01:34:33 +0530

    Create snapshot for app component using ReactTestRenderer

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -7,7 +7,7 @@ import ReactTestRenderer from 'react-test-renderer';
 import App from '../App';
 
 test('renders correctly', async () => {
-  await ReactTestRenderer.act(() => {
-    ReactTestRenderer.create(<App />);
+  const tree = ReactTestRenderer.create(<App />).toJSON();
+  expect(tree).toMatchSnapshot();
   });
-});
+
