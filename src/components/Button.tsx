c25af507b03dd9a67829b654bb3ee83b9be54f39
nagusha <nagusha.madasu@everest.engineering>
2025-05-06 01:52:14 +0530

    Apply button and text styles to components

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -13,8 +13,8 @@ const Button = ({title, onPress} : ButtonProps) => {
     const colors = useThemeColors();
     const styles = getStyles(colors);
   return (
-    <TouchableOpacity onPress={onPress}>
-      <Text>{title}</Text>
+    <TouchableOpacity style={styles.button} onPress={onPress}>
+      <Text style={styles.text}>{title}</Text>
     </TouchableOpacity>
   )
 }
@@ -26,7 +26,7 @@ const getStyles = (colors: any) =>
         width: width * 0.45,
         borderRadius: 4,
         justifyContent: 'center',
-        top: 290,
+        top: 300,
         alignItems: 'center',
         backgroundColor: colors.primaryBlue,
         marginVertical: 5,
