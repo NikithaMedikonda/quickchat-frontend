82892b446f4306429f31e67a663a56b47def3c6e
nagusha <nagusha.madasu@everest.engineering>
2025-05-06 00:40:50 +0530

    Add dynamic button styles using getStyles function with theme colors

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,4 @@
-import { TouchableOpacity, Text } from 'react-native'
+import { TouchableOpacity, Text,  StyleSheet, Dimensions} from 'react-native'
 import React from 'react'
 
 type ButtonProps = {
@@ -7,6 +7,8 @@ type ButtonProps = {
   };
   
 const Button = ({title, onPress} : ButtonProps) => {
+    const colors = useThemeColors();
+    const styles = getStyles(colors);
   return (
     <TouchableOpacity onPress={onPress}>
       <Text>{title}</Text>
@@ -15,3 +17,17 @@ const Button = ({title, onPress} : ButtonProps) => {
 }
 
 export default Button
+
+const getStyles = (colors: any) =>
+    StyleSheet.create({
+      button: {
+        height: 40,
+        width: width * 0.45,
+        borderRadius: 4,
+        justifyContent: 'center',
+        top: 290,
+        alignItems: 'center',
+        backgroundColor: colors.primaryBlue,
+        marginVertical: 5,
+      },
+    })
