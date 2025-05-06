c563e3251fcdb365ba7b53b3d1e2867b570ff7cd
nagusha <nagusha.madasu@everest.engineering>
2025-05-06 11:36:40 +0530

    Add lint format to WelcomeScreen

--- src/screens/WelcomeScreen/WelcomeScreen.tsx
+++ src/screens/WelcomeScreen/WelcomeScreen.tsx
@@ -1,53 +1,51 @@
-import { View, StyleSheet, Image, Dimensions } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { View, StyleSheet, Image, Dimensions } from 'react-native';
 import { useNavigate } from 'react-router-native';
-import {Button}  from '../../components/Button';
+import { Button } from '../../components/Button';
 import { useThemeColors } from '../../constants/color';
-const { width,height } = Dimensions.get('window');
 
+const { width, height } = Dimensions.get('window');
 
 export const WelcomeScreen = () => {
     const navigate = useNavigate();
     const colors = useThemeColors();
     const styles = getStyles(colors);
 
-
     return (
         <View style={styles.container}>
             <View style={styles.imageContainer}>
-            <Image style={styles.image} source={require('../../assets/images/quickchat_logo.png')}
-                testID="logo-image"
-            />
+                <Image style={styles.image} source={require('../../assets/images/quickchat_logo.png')}
+                    testID="logo-image"
+                />
             </View>
             <View >
-            <Button title="Get Started" onPress={() => navigate('/register')}/>
+                <Button title="Get Started" onPress={() => navigate('/register')} />
             </View>
-         
         </View>
-    )
-}
+    );
+};
 
 export const getStyles = (colors: any) =>
-  StyleSheet.create({
-    container: {
-        display:"flex",
-        justifyContent:"center",
-        alignItems: "center",
-        backgroundColor:colors.background,
-        height:height,
-        width:width,
+    StyleSheet.create({
+        container: {
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: colors.background,
+            height: height,
+            width: width,
 
-    },
-    image: {
-        width: width*0.5,
-        height: height*0.3,
-    },
+        },
+        image: {
+            width: width * 0.5,
+            height: height * 0.3,
+        },
 
-    imageContainer:{
-        height:height*0.6,
-        width:width*0.9,
-        display:"flex",
-        justifyContent:"center",
-        alignItems:"center"
-    }
-  });
+        imageContainer: {
+            height: height * 0.6,
+            width: width * 0.9,
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+        },
+    });
