40399bdfb385da69ec72611ab2d7bf0a331d57a6
vinaysaichelupuri <vinaysai.chelupuri@everest.engineering>
2025-05-06 17:22:00 +0530

    Update the App.tsx with testing

--- App.tsx
+++ App.tsx
@@ -1,18 +1,7 @@
 import React from 'react';
-import { SafeAreaView, Text, StyleSheet, Dimensions } from 'react-native';
-import { Registration } from './src/screens/Registration/Registration.tsx';
+import {Registration} from './src/screens/Registration/Registration.tsx';
 export const App = () => {
-  return (
-    <SafeAreaView style={styles.container}>
-      <Registration/>
-    </SafeAreaView>
-  );
+  return <Registration />;
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
+
