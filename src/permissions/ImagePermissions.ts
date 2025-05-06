ef51eabc3e45d64ae8a6ae07545d86dbbf45846c
vinaysaichelupuri <vinaysai.chelupuri@everest.engineering>
2025-05-06 17:13:32 +0530

    Implement permission for image

---
+++ src/permissions/ImagePermissions.ts
@@ -0,0 +1,71 @@
+import {Platform} from 'react-native';
+import {check, PERMISSIONS, request, RESULTS} from 'react-native-permissions';
+
+export const requestPermissions = async () => {
+  if (Platform.OS === 'android') {
+    try {
+      // Check the status of each permission
+      const checkCamera = await check(PERMISSIONS.ANDROID.CAMERA);
+      const checkExternalRead = await check(
+        PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE,
+      );
+
+      // If any permission is denied, request the denied permissions
+      let permissionsDenied = false;
+
+      if (checkCamera === RESULTS.DENIED) {
+        const grantedCamera = await request(PERMISSIONS.ANDROID.CAMERA);
+        if (grantedCamera !== RESULTS.GRANTED) {
+          permissionsDenied = true;
+        }
+      }
+
+      if (checkExternalRead === RESULTS.DENIED) {
+        const grantedExternalRead = await request(
+          PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE,
+        );
+        if (grantedExternalRead !== RESULTS.GRANTED) {
+          permissionsDenied = true;
+        }
+      }
+      return permissionsDenied;
+    } catch (e) {
+      console.log(e);
+    }
+  } else if (Platform.OS === 'ios') {
+    try {
+      const cameraStatus = await check(PERMISSIONS.IOS.CAMERA);
+      const photoLibraryStatus = await check(PERMISSIONS.IOS.PHOTO_LIBRARY);
+      const photoLibraryAddStatus = await check(
+        PERMISSIONS.IOS.PHOTO_LIBRARY_ADD_ONLY,
+      );
+      let permissionsDenied = false;
+
+      if (cameraStatus === RESULTS.DENIED || RESULTS.LIMITED) {
+        const grantedCamera = await request(PERMISSIONS.IOS.CAMERA);
+        if (grantedCamera !== RESULTS.GRANTED) {
+          permissionsDenied = true;
+        }
+      }
+
+      if (photoLibraryStatus === RESULTS.DENIED || RESULTS.LIMITED) {
+        const grantedExternal = await request(PERMISSIONS.IOS.PHOTO_LIBRARY);
+        if (grantedExternal !== RESULTS.GRANTED) {
+          permissionsDenied = true;
+        }
+      }
+
+      if (photoLibraryAddStatus === RESULTS.DENIED || RESULTS.LIMITED) {
+        const grantedExternalRead = await request(
+          PERMISSIONS.IOS.PHOTO_LIBRARY_ADD_ONLY,
+        );
+        if (grantedExternalRead !== RESULTS.GRANTED) {
+          permissionsDenied = true;
+        }
+      }
+      return permissionsDenied;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+};
