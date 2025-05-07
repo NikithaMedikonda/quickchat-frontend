27d8f3df8e5197e57acf8446e649adce020c0985
NikithaMedikonda <nikitha.medikonda@everest.engineering>
2025-05-07 15:48:34 +0530

    Update App.tsx to integrate loading component functionality

--- App.tsx
+++ App.tsx
@@ -1,23 +1,25 @@
-import React, { useEffect } from 'react';
+import React, {useEffect} from 'react';
 import SplashScreen from 'react-native-splash-screen';
-import { NativeRouter, Routes, Route } from 'react-router-native';
+import {NativeRouter, Routes, Route} from 'react-router-native';
 import {WelcomeScreen} from './src/screens/WelcomeScreen/WelcomeScreen';
 import RegisterScreen from './src/screens/RegisterScreen/RegisterScreen';
-
+import {Provider} from 'react-redux';
+import {store} from './src/store/store';
 
 const App = () => {
   useEffect(() => {
     SplashScreen.hide();
   }, []);
   return (
-    <NativeRouter>
-      <Routes>
-        <Route path="/" element={<WelcomeScreen />} />
-        <Route path="register" element={<RegisterScreen />} />
-      </Routes>
-    </NativeRouter>
+    <Provider store={store}>
+      <NativeRouter>
+        <Routes>
+          <Route path="/" element={<WelcomeScreen />} />
+          <Route path="register" element={<RegisterScreen />} />
+        </Routes>
+      </NativeRouter>
+    </Provider>
   );
 };
 
 export default App;
-
