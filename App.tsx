ac7011fe6cd7814e0ed821bd89bf9a11f2b8eb31
ushasri2645 <ugudikandula@everest.engineering>
2025-05-07 18:45:07 +0530

    Detect language change on the start of app.

--- App.tsx
+++ App.tsx
@@ -3,12 +3,18 @@ import SplashScreen from 'react-native-splash-screen';
 import { NativeRouter, Routes, Route } from 'react-router-native';
 import {WelcomeScreen} from './src/screens/WelcomeScreen/WelcomeScreen';
 import RegisterScreen from './src/screens/RegisterScreen/RegisterScreen';
+import './src/i18n/i18n.config'
+import * as RNLocalize from 'react-native-localize';
+import {i18next} from './src/i18n/i18n.config';
 
 
-const App = () => {
+export const App = () => {
   useEffect(() => {
     SplashScreen.hide();
+    const languageCode = RNLocalize.getLocales()[0].languageCode || 'en';
+    i18next.changeLanguage(languageCode);
   }, []);
+
   return (
     <NativeRouter>
       <Routes>
@@ -19,5 +25,5 @@ const App = () => {
   );
 };
 
-export default App;
+ 
 
