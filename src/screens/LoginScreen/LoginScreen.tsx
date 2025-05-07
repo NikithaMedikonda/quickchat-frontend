43b69d3f1578b07d2448f897ded2911289e7a1ab
AnushaUppuu <anusha.uppu@everest.engineering>
2025-05-07 18:38:51 +0530

    Create the Login screen

---
+++ src/screens/LoginScreen/LoginScreen.tsx
@@ -0,0 +1,70 @@
+import React, {useState} from 'react';
+import {Image, Text, TouchableOpacity, View} from 'react-native';
+
+import {Button} from '../../components/Button/Button';
+import {Placeholder} from '../../components/InputField/InputField';
+
+import {loginStyles} from './Login.styles';
+import {useThemeColors} from '../../constants/color';
+import {useNavigate} from 'react-router-native';
+import {useNavigation} from '@react-navigation/native';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+
+type RootStackParamList = {
+  register: undefined;
+};
+
+export type RegisterScreenNavigationProp = NativeStackNavigationProp<
+  RootStackParamList,
+  'register'
+>;
+function LoginScreen() {
+  const colors = useThemeColors();
+  const styles = loginStyles(colors);
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [password, setPassword] = useState('');
+  const naviagte = useNavigation<RegisterScreenNavigationProp>();
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.image}
+        source={require('./../../../assets/quickchat.png')}
+        accessibilityHint="logo-image"
+      />
+      <Placeholder
+        title="Phone number"
+        value={phoneNumber}
+        onChange={value => {
+          setPhoneNumber(value);
+        }}
+        secureTextEntry={false}
+      />
+      {phoneNumber !== '' && phoneNumber.length !== 10 && (
+        <View style={styles.validationView}>
+          <Text style={styles.validationText}>Phone number not exists!</Text>
+        </View>
+      )}
+      <Placeholder
+        title="Password"
+        value={password}
+        onChange={value => {
+          setPassword(value);
+        }}
+        secureTextEntry={true}
+      />
+      <Button title="Login" onPress={() => {}} />
+      <View style={styles.messageView}>
+        <Text style={styles.messageText}>Don't have an account?</Text>
+        <TouchableOpacity
+          style={styles.signUpContainer}
+          onPress={() => {
+            naviagte.navigate('register');
+          }}>
+          <Text style={styles.validationText}>Sign up</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+export default LoginScreen;
