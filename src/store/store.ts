715f9d0f109c40e3846c70c590983d1cd8da2366
NikithaMedikonda <nikitha.medikonda@everest.engineering>
2025-05-07 16:32:38 +0530

    Add registration slice

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import loadingReducer from './slices/loadingSlice';
+import registrationReducer from './slices/registrationSlice';
 
 export const store = configureStore({
   reducer: {
     loading: loadingReducer,
+    registration: registrationReducer,
   },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
