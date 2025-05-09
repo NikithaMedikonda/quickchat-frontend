1b542cfea002092437dde6ca050230c1a617120b
vinaysaichelupuri <vinaysai.chelupuri@everest.engineering>
2025-05-09 15:24:22 +0530

    Add new services directory for backend/API utilities

---
+++ src/services/RegisterUser.ts
@@ -0,0 +1,33 @@
+import { API_URL } from '../../api';
+
+export const registerUser = async (payload: {
+  image: string;
+  firstName: string;
+  lastName: string;
+  phoneNumber: string;
+  password: string;
+  email: string;
+}) => {
+  const userData = {
+    phoneNumber: payload.phoneNumber,
+    firstName: payload.firstName,
+    lastName: payload.lastName,
+    profilePicture: payload.image,
+    email: payload.email,
+    password: payload.password,
+  };
+  const response = await fetch(`${API_URL}/api/users`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(userData),
+  });
+ console.log(response)
+  const data = await response.json();
+  console.log(data);
+  return {
+    status: response.status,
+    data,
+  };
+};
