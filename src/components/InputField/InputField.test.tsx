66ea5bc0eb5ca95801dc86272c17a6d702cebad8
vinaysaichelupuri <vinaysai.chelupuri@everest.engineering>
2025-05-09 15:24:04 +0530

    Add new InputField component

---
+++ src/components/InputField/InputField.test.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {render} from '@testing-library/react-native';
+import {Placeholder} from './InputField';
+
+describe('Input Field', () => {
+  it('renders input fields', () => {
+    const {getByPlaceholderText, getByDisplayValue} = render(
+      <Placeholder
+        title="First Name"
+        value="TestUser"
+        secureTextEntry={false}
+        onChange={() => {}}
+      />,
+    );
+    expect(getByPlaceholderText('First Name')).toBeTruthy();
+    expect(getByDisplayValue('TestUser')).toBeTruthy();
+  });
+});
