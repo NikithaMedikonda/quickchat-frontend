f1ddfb4af2bec47f7eff0bcfac5daa90d14fd98c
vinaysaichelupuri <vinaysai.chelupuri@everest.engineering>
2025-05-09 15:22:04 +0530

    Update App.tsx with latest component structure and imports

--- App.tsx
+++ App.tsx
@@ -1,24 +1,26 @@
 import React, {useEffect} from 'react';
 import SplashScreen from 'react-native-splash-screen';
 import {NativeRouter, Routes, Route} from 'react-router-native';
-import {WelcomeScreen} from './src/screens/WelcomeScreen/WelcomeScreen';
-import {Registration} from './src/screens/Registration/Registration.tsx';
 import {Provider} from 'react-redux';
+import { WelcomeScreen } from './src/screens/Welcome/Welcome';
+import { Registration } from './src/screens/Registration/Registration';
 import {store} from './src/store/store';
+import {LoadingComponent} from './src/components/Loading/Loading';
 
-const App = () => {
+export const App = () => {
   useEffect(() => {
     SplashScreen.hide();
   }, []);
   return (
     <Provider store={store}>
     <NativeRouter>
+      <LoadingComponent/>
       <Routes>
         <Route path="/" element={<WelcomeScreen />} />
-        <Route path="register" element={<Registration/>} />
+        <Route path="/register" element={<Registration/>} />
       </Routes>
     </NativeRouter>
-  </Provider>  );
-};
+  </Provider>
 
-export default App;
+  );
+};
