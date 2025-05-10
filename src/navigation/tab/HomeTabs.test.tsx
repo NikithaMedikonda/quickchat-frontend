9bb05649c865d4b1f0e9a407a931a24459ac6086
ushasri2645 <ugudikandula@everest.engineering>
2025-05-10 22:00:02 +0530

    Adjust import statements.

--- src/navigation/tab/HomeTabs.test.tsx
+++ src/navigation/tab/HomeTabs.test.tsx
@@ -1,6 +1,6 @@
-import {HomeTabs} from './HomeTabs';
 import {NavigationContainer} from '@react-navigation/native';
 import {render} from '@testing-library/react-native';
+import {HomeTabs} from './HomeTabs';
 
 describe('Welcome Screen', () => {
   it('renders the Tabs', () => {
