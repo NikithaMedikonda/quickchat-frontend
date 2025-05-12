541ee758a3551471950f4fc03b92b31c41a36225
AnushaUppuu <anusha.uppu@everest.engineering>
2025-05-12 17:06:02 +0530

    Update the test file as of the change in the login file

--- src/screens/LoginScreen/LoginScreen.test.tsx
+++ src/screens/LoginScreen/LoginScreen.test.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
 import {
   fireEvent,
   render,
   screen,
   waitFor,
 } from '@testing-library/react-native';
+import React from 'react';
 import LoginScreen from './LoginScreen';
 
-const mockNavigate = jest.fn();
 jest.mock('@react-navigation/native', () => ({
   useNavigation: () => ({
     navigate: mockNavigate,
   }),
 }));
+const mockNavigate = jest.fn();
+
+jest.mock('react-native-phone-input');
+
 describe('Login Screen', () => {
   beforeEach(() => {
     render(<LoginScreen />);
   });
   test('should render the elements correctly', () => {
-    expect(screen.getByPlaceholderText('Phone number')).toBeOnTheScreen();
     expect(screen.getByPlaceholderText('Password')).toBeOnTheScreen();
     expect(screen.getByText('Login')).toBeOnTheScreen();
     expect(screen.getByText('Sign up')).toBeOnTheScreen();
@@ -29,15 +31,7 @@ describe('Login Screen', () => {
     fireEvent.press(signUp);
     expect(mockNavigate).toHaveBeenCalledWith('register');
   });
-  test('should change the value of phone number upon entering', async () => {
-    const phoneNumber = screen.getByPlaceholderText('Phone number');
 
-    fireEvent.changeText(phoneNumber, {target: {value: '9866342189'}});
-
-    await waitFor(() => {
-      expect(phoneNumber.props.value.target.value).toBe('9866342189');
-    });
-  });
   test('should change the value of password upon entering', async () => {
     const phoneNumber = screen.getByPlaceholderText('Password');
 
