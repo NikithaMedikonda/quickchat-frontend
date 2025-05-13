7f7e2ec82efac6aa293d2482fb2caa36debad4e3
ushasri2645 <ugudikandula@everest.engineering>
2025-05-13 14:08:09 +0530

    Add styles for contact component

---
+++ src/components/Contact/Contact.styles.ts
@@ -0,0 +1,45 @@
+import {Dimensions, StyleSheet} from 'react-native';
+import {Colors} from '../../constants/colors';
+
+const {width, height} = Dimensions.get('window');
+
+export const getStyles = (colors: Colors) =>
+  StyleSheet.create({
+    contactContainer:{
+        flex:1,
+        backgroundColor:colors.background,
+        left:0.05*width,
+        display:'flex',
+        flexDirection:'row',
+        justifyContent:'flex-start',
+    },
+    loadingContactsDisplay: {
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems:'center',
+      justifyContent:'center',
+      flex:1,
+    },
+    loadingContactsText:{
+        color:colors.white
+    },
+    text:{
+        color:colors.white,
+        left:0.05*width,
+        fontWeight:'bold'
+    },
+    image:{
+        height:35,
+        width:35
+    },
+    invitationContainer:{
+        display:'flex',
+        // flexDirection:'column',
+        justifyContent:'center',
+        // alignSelf:'flex-end',
+        left:0.4*width,
+    },
+    inviteText:{
+        color:colors.primaryBlue
+    }
+  });
