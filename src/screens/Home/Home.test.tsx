0bd6f2c0449c7dc748b7fe5655d9baca3a0b1007
nagusha <nagusha.madasu@everest.engineering>
2025-05-13 13:39:18 +0530

    Remove the only name in test cases

--- src/screens/Home/Home.test.tsx
+++ src/screens/Home/Home.test.tsx
@@ -1,12 +1,12 @@
 import {render, screen} from '@testing-library/react-native';
 import { Home } from './Home';
 
-describe.only('Home Screen', () => {
-  it.only('renders the description', () => {
+describe('Home Screen', () => {
+  it('renders the description', () => {
     const { getByText } = render(<Home/>);
     expect(getByText('You have no chats. Start Messaging!')).toBeTruthy();
   });
-  it.only('render the plus-image icon', () => {
+  it('render the plus-image icon', () => {
     render(< Home/>);
         const image = screen.getByA11yHint('plus-image');
         expect(image.props.source).toEqual({
