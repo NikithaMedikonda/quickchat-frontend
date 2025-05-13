280858167585a29a515a50c2e943de0a3a727ce0
nagusha <nagusha.madasu@everest.engineering>
2025-05-13 13:20:26 +0530

    Update profile-Image to plus-image in test

--- src/screens/Home/Home.test.tsx
+++ src/screens/Home/Home.test.tsx
@@ -1,14 +1,14 @@
 import {render, screen} from '@testing-library/react-native';
 import { Home } from './Home';
 
-describe('Home Screen', () => {
-  it('renders the description', () => {
+describe.only('Home Screen', () => {
+  it.only('renders the description', () => {
     const { getByText } = render(<Home/>);
     expect(getByText('You have no chats. Start Messaging!')).toBeTruthy();
   });
-  it('render the plus-image icon', () => {
+  it.only('render the plus-image icon', () => {
     render(< Home/>);
-        const image = screen.getByA11yHint('profile-image');
+        const image = screen.getByA11yHint('plus-image');
         expect(image.props.source).toEqual({
             testUri: '../../src/assets/plus-icon.png',
         });
