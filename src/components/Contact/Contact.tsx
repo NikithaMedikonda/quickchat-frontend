28de692312c99df55092d0cfe90caf09c2f06d80
ushasri2645 <ugudikandula@everest.engineering>
2025-05-13 14:01:58 +0530

    Add static contact component

---
+++ src/components/Contact/Contact.tsx
@@ -0,0 +1,34 @@
+import {View, Text, Image} from 'react-native';
+import {ContactDetails} from '../../types/contact.types';
+import {DEFAULT_PROFILE_IMAGE} from '../../constants/defaultImage';
+import {useThemeColors} from '../../constants/colors';
+import {getStyles} from './Contact.styles';
+
+export const Contact = ({contactDetails}: {contactDetails: ContactDetails}) => {
+  const colors = useThemeColors();
+  const styles = getStyles(colors);
+  return (
+    <View style={styles.contactContainer}>
+      <View>
+        {contactDetails.toBeInvited ? (
+          <Image style={styles.image} source={{uri: DEFAULT_PROFILE_IMAGE}} accessibilityHint='default-image' />
+        ) : (
+          <Image
+            style={styles.image}
+            source={{uri: contactDetails.profilePic}}
+            accessibilityHint='profile-image'
+          />
+        )}
+      </View>
+      <View>
+        <Text style={styles.text}>{contactDetails.name}</Text>
+        <Text style={styles.text}>{contactDetails.phoneNumber}</Text>
+      </View>
+      {contactDetails.toBeInvited && (
+        <View style={styles.invitationContainer}>
+          <Text style={styles.inviteText}>Invite</Text>
+        </View>
+      )}
+    </View>
+  );
+};
