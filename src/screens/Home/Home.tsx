1dedf28c178bf2f432f0510342dccac9b8bf61a8
nagusha <nagusha.madasu@everest.engineering>
2025-05-13 13:00:01 +0530

    Add home in text tag and image tag

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,9 +1,23 @@
-import {Text, View} from 'react-native';
+import { Image, Text, View } from 'react-native';
+import { getStyles } from './Home.styles';
+import { useThemeColors } from '../../constants/colors';
 
 export const Home = () => {
+  const colors = useThemeColors();
+  const styles = getStyles(colors);
   return (
-    <View>
-      <Text>Hello home</Text>
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.description}>You have no chats. Start Messaging!</Text>
+      </View>
+      <View style={styles.innerContainer}>
+       <Image
+        style={styles.plusImage}
+        source={require('../../assets/plus-icon.png')}
+        accessibilityHint="plus-image"
+       />
+      </View>
     </View>
   );
 };
+
