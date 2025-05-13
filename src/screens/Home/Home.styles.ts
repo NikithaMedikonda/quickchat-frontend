d52582d362496d7ba59e56064415cc98313e299a
nagusha <nagusha.madasu@everest.engineering>
2025-05-13 12:56:38 +0530

    Modify home styles

---
+++ src/screens/Home/Home.styles.ts
@@ -0,0 +1,31 @@
+import { StyleSheet, Dimensions } from 'react-native';
+import { Colors } from '../../constants/colors';
+
+const { width, height } = Dimensions.get('window');
+
+export const getStyles = (colors: Colors) =>
+    StyleSheet.create({
+        container: {
+            display: 'flex',
+            height: height,
+            width: width,
+            backgroundColor: colors.background,
+            paddingTop: width * 1.05,
+        },
+        description: {
+            color: colors.gray,
+            fontSize: 18,
+            fontWeight: 'bold',
+            textAlign: 'center',
+        },
+        innerContainer: {
+            alignItems: 'flex-end',
+            width: width * 0.90,
+            marginTop: height * 0.20,
+            height: height * 0.90,
+        },
+        plusImage: {
+            width: width * 0.18,
+            height: height * 0.14,
+        },
+    });
