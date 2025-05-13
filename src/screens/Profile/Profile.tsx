6768097cfb9a034c27d9826b8229a61e5eb6df3d
jangamkeerthi26 <keerthi.jangam@everest.engineeing>
2025-05-13 11:23:35 +0530

    Add EditProfile navigation

--- src/screens/Profile/Profile.tsx
+++ src/screens/Profile/Profile.tsx
@@ -1,9 +1,19 @@
-import {Text, View} from 'react-native';
+import {Text, View, TouchableOpacity} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import {ProfileScreenNavigationProp} from '../../types/usenavigation.type';
 
 export const Profile = () => {
+  const navigation = useNavigation<ProfileScreenNavigationProp>();
+
+  const goToEditProfile = () => {
+    navigation.navigate('editProfile');
+  };
   return (
     <View>
       <Text>Hello profile</Text>
+      <TouchableOpacity onPress={goToEditProfile}>
+        <Text>Edit Profile</Text>
+      </TouchableOpacity>
     </View>
   );
 };
