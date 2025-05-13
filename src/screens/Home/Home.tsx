563e6a53a6e3f3471049c39e6f6d7c3a881fe066
nagusha <nagusha.madasu@everest.engineering>
2025-05-13 13:49:31 +0530

    Update home in useTranslation

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,14 +1,16 @@
 import { Image, Text, View } from 'react-native';
 import { getStyles } from './Home.styles';
 import { useThemeColors } from '../../constants/colors';
+import { useTranslation } from 'react-i18next';
 
 export const Home = () => {
   const colors = useThemeColors();
+  const {t} = useTranslation('auth');
   const styles = getStyles(colors);
   return (
     <View style={styles.container}>
       <View>
-        <Text style={styles.description}>You have no chats. Start Messaging!</Text>
+        <Text style={styles.description}>{t('You have no chats. Start Messaging!')}</Text>
       </View>
       <View style={styles.innerContainer}>
        <Image
