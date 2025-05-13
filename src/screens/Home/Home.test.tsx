939b28d7b7c07ffe2656b0dea5d940f36aa606af
nagusha <nagusha.madasu@everest.engineering>
2025-05-13 12:54:13 +0530

    Modify home screen test cases

--- src/screens/Home/Home.test.tsx
+++ src/screens/Home/Home.test.tsx
@@ -1,10 +1,16 @@
 import {render, screen} from '@testing-library/react-native';
 import { Home } from './Home';
 
-describe('Profile Screen', () => {
-  it('renders the logo image', () => {
-    render(<Home />);
-    expect(screen.getByText('Hello home')).toBeTruthy();
+describe.only('Home Screen', () => {
+  it.only('renders the description', () => {
+    const { getByText } = render(<Home/>);
+    expect(getByText('You have no chats. Start Messaging!')).toBeTruthy();
+  });
+  it.only('render the plus-image icon', () => {
+    render(< Home/>);
+        const image = screen.getByA11yHint('profile-image');
+        expect(image.props.source).toEqual({
+            testUri: '../../src/assets/plus-icon.png',
+        });
   });
 });
-
