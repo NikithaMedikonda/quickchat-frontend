ac05f6a36cd7d0dc394c76e7d78d1b96f59179c4
goldie <goldie@Anoosha-Sanugula.local>
2025-05-14 10:22:16 +0530

    Implement delete user service

--- src/services/DeleteUser.ts
+++ src/services/DeleteUser.ts
@@ -1,15 +1,18 @@
 import {API_URL} from '../constants/api';
 
-export const deleteUser = async (phoneNumber: string) => {
+export const deleteUser = async (payload: {
+  phoneNumber: string;
+  authToken: string;
+}) => {
   const response = await fetch(`${API_URL}/api/deleteAccount`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
+      authorization: `Bearer ${payload.authToken}`,
     },
-    body: JSON.stringify(phoneNumber),
+    body: JSON.stringify({phoneNumber: payload.phoneNumber}),
   });
   const data = await response.json();
-  console.log('data in the delete user');
   return {
     status: response.status,
     data,
