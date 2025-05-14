b0cbc909eaf9abc0161dd1f737ef2eccaa9e71b2
ushasri2645 <ugudikandula@everest.engineering>
2025-05-14 11:37:54 +0530

    Add tests for homescreen

--- src/screens/Home/Home.test.tsx
+++ src/screens/Home/Home.test.tsx
@@ -1,10 +1,55 @@
-import {render, screen} from '@testing-library/react-native';
-import { Home } from './Home';
+import {render, screen, fireEvent} from '@testing-library/react-native';
+import {Home} from './Home';
+import {Provider} from 'react-redux';
+import {store} from '../../store/store';
+import { useThemeColors } from '../../constants/colors';
 
-describe('Profile Screen', () => {
-  it('renders the logo image', () => {
-    render(<Home />);
-    expect(screen.getByText('Hello home')).toBeTruthy();
+const mockSetOptions = jest.fn();
+const mockNavigate = jest.fn();
+
+jest.mock('@react-navigation/native', () => ({
+  useNavigation: () => ({
+    setOptions: mockSetOptions,
+    navigate: mockNavigate,
+  }),
+}));
+
+
+describe('Home Screen Tests', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('sets the header options using useLayoutEffect', () => {
+    render(
+      <Provider store={store}>
+        <Home />
+      </Provider>,
+    );
+
+    expect(mockSetOptions).toHaveBeenCalledWith({
+      headerTitle: 'Quick Chat',
+      headerTitleAlign: 'center',
+      headerStyle: {
+        backgroundColor: useThemeColors().background,
+      },
+      headerTitleStyle: {
+        color: useThemeColors().white,
+      },
+    });
+  });
+
+  it('renders Hello home text and navigates on press', () => {
+    render(
+      <Provider store={store}>
+        <Home />
+      </Provider>,
+    );
+
+    const text = screen.getByText('Hello home');
+    expect(text).toBeTruthy();
+
+    fireEvent.press(text);
+    expect(mockNavigate).toHaveBeenCalledWith('contacts');
   });
 });
-
