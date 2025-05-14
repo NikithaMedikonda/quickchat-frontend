e03de2ad3982b8151286b628d7bce0fdb9583c41
ushasri2645 <ugudikandula@everest.engineering>
2025-05-14 11:38:14 +0530

    Implement custom header

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,6 +1,25 @@
-import {Text, View} from 'react-native';
+import {useLayoutEffect} from 'react';
+import {useNavigation} from '@react-navigation/native';
+import {Text,View} from 'react-native';
+import {HomeTabsProps} from '../../types/usenavigation.type';
+
+import {useThemeColors} from '../../constants/colors';
 
 export const Home = () => {
+  const navigation = useNavigation<HomeTabsProps>();
+  const colors = useThemeColors();
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerTitle: 'Quick Chat',
+      headerTitleAlign: 'center',
+      headerStyle: {
+        backgroundColor: colors.background,
+      },
+      headerTitleStyle: {
+        color: colors.white,
+      },
+    });
+  });
   return (
     <View>
       <Text>Hello home</Text>
