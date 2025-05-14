c81e73a3ed6ac9a29d4ae43fee47be2507e26bfb
ushasri2645 <ugudikandula@everest.engineering>
2025-05-14 21:19:51 +0530

    Send token from frontend and adjust route name

--- src/services/GetContacts.ts
+++ src/services/GetContacts.ts
@@ -1,5 +1,6 @@
 import Contacts from 'react-native-contacts';
 import {API_URL} from '../constants/api';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const getContacts = async () => {
   const phoneContacts = await Contacts.getAll();
@@ -12,17 +13,23 @@ export const getContacts = async () => {
   });
 
   try {
-    const response = await fetch(`${API_URL}/api/contacts`, {
+    const authToken = await AsyncStorage.getItem('authToken');
+    console.log(authToken);
+    if (!authToken) {
+      throw new Error('Authorization failed');
+    }
+    const response = await fetch(`${API_URL}/api/users/contacts`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
+        authorization: `Bearer ${authToken}`,
       },
       body: JSON.stringify(allContacts),
     });
     const data = await response.json();
     return {
       status: response.status,
-      data,
+      data: data.data,
     };
   } catch (error) {
     throw new Error(`${error}`);
