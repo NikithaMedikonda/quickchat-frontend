7be7637f6252c0f12116d59e43a36bb407f53f82
nagusha <nagusha.madasu@everest.engineering>
2025-05-15 14:11:09 +0530

    Modify the TimeStamp styles

--- src/components/TimeStamp/TimeStamp.styles.ts
+++ src/components/TimeStamp/TimeStamp.styles.ts
@@ -1,8 +1,8 @@
 import { StyleSheet } from 'react-native';
-import {Colors} from '../../constants/colors';
-export const Timestampstyle = (colors: Colors)=>
-    StyleSheet.create({
-       color:{
-         color :colors.text,
-    },
-});
+import { Colors } from '../../constants/colors';
+export const Timestampstyle = (colors: Colors) =>
+   StyleSheet.create({
+      color: {
+         color: colors.text,
+      },
+   });
