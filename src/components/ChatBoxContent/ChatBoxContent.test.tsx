8c2c9b9cbee1865387a13f33617f52279c6e4046
nagusha <nagusha.madasu@everest.engineering>
2025-05-15 14:07:57 +0530

    Modify image and props name in test cases

--- src/components/ChatBoxContent/ChatBoxContent.test.tsx
+++ src/components/ChatBoxContent/ChatBoxContent.test.tsx
@@ -6,12 +6,12 @@ describe('ChatBoxContent', () => {
         render(< ChatBoxContent name="User" description="Hello" />);
         const image = screen.getByA11yHint('profile-image');
         expect(image.props.source).toEqual({
-            testUri: '../../../src/assets/profile-image.png',
+            uri: 'https://sdjetntpocezxjoelxgb.supabase.co/storage/v1/object/public/quick-chat/images/profile-pics/image.png',
         });
     });
     it('render the headerContainer', () => {
-        const { getByText } = render(< ChatBoxContent name="User" description="Hello" />);
-        expect(getByText('User')).toBeTruthy();
-        expect(getByText('Hello')).toBeTruthy();
+        const { getByText } = render(< ChatBoxContent name="Nagulu" description="Hello everyone" />);
+        expect(getByText('Nagulu')).toBeTruthy();
+        expect(getByText('Hello everyone')).toBeTruthy();
     });
 });
