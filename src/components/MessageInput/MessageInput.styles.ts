d33d0623077f8863422050f2d7b9dc27c59cbfc3
durgamkeerthana <keerthana.durgam@everest.engineering>
2025-05-20 14:20:34 +0530

    Modify the Input box styles to align in center

--- src/components/MessageInput/MessageInput.styles.ts
+++ src/components/MessageInput/MessageInput.styles.ts
@@ -20,7 +20,7 @@ export const messageInputStyles = (colors: Colors) => {
       width: width * 0.7,
       padding: 4,
       color: colors.gray,
-      margin: height * 0.01,
+      margin: height * 0.02,
       backgroundColor: colors.white,
       alignSelf: 'flex-end',
     },
