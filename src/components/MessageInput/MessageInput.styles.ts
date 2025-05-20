3c1eaed09e1ab9bd690d7902ac34476763230b46
durgamkeerthana <keerthana.durgam@everest.engineering>
2025-05-20 11:39:12 +0530

    Implement message input component with appropriate styling and testing

---
+++ src/components/MessageInput/MessageInput.styles.ts
@@ -0,0 +1,36 @@
+import {Dimensions, StyleSheet} from 'react-native';
+import {Colors} from '../../constants/colors';
+
+const {width, height} = Dimensions.get('window');
+
+export const messageInputStyles = (colors: Colors) => {
+  return StyleSheet.create({
+    messageInput: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      paddingHorizontal: 10,
+      paddingVertical: 8,
+      backgroundColor: colors.background,
+      marginBottom: 15,
+    },
+    inputbox: {
+      height: height * 0.055,
+      fontSize: 20,
+      borderRadius: 12,
+      width: width * 0.7,
+      padding: 4,
+      color: colors.gray,
+      margin: height * 0.010,
+      backgroundColor: colors.white,
+      alignSelf: 'flex-end',
+    },
+    placeholder: {
+      color: colors.gray,
+    },
+    sendIcon: {
+      width: width * 0.16,
+      height: width * 0.16,
+      resizeMode: 'contain',
+    },
+  });
+};
