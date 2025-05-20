6253601b04b087cd2cee69e90ef65e1da60cfb8b
durgamkeerthana <keerthana.durgam@everest.engineering>
2025-05-20 15:16:12 +0530

    Remove the sent and received messages from Individual chat screen

--- src/screens/IndividualChat/IndividualChat.tsx
+++ src/screens/IndividualChat/IndividualChat.tsx
@@ -1,22 +1,13 @@
-import { useState } from 'react';
-import { ScrollView, View } from 'react-native';
+import { View } from 'react-native';
 import { IndividualChatHeader } from '../../components/IndividualChatHeader/IndividualChatHeader';
 import { MessageInput } from '../../components/MessageInput/MessageInput';
-import { ReceivedMessage } from '../../components/ReceivedMessage/ReceivedMessage';
-import { SentMessage } from '../../components/SentMessage/SentMessage';
 import { useThemeColors } from '../../constants/colors';
-import { Message, ReceivedMessagetype } from '../../types/messsage.types';
 import { UserDetails } from '../../types/user.types';
 import { individualChatstyles } from './IndividualChat.styles';
 
 export const IndividualChat = ({user}: {user: UserDetails}) => {
   const colors = useThemeColors();
   const styles = individualChatstyles(colors);
-
-  const [sentMessages, setSentMessages] = useState<Message[]>([]);
-  const [receivedMessages, setReceivedMessages] = useState<
-    ReceivedMessagetype[]
-  >([]);
   return (
     <View style={styles.container}>
       <View>
@@ -25,10 +16,6 @@ export const IndividualChat = ({user}: {user: UserDetails}) => {
           profilePicture={user.profilePicture}
         />
       </View>
-      <ScrollView accessibilityHint="message-container">
-        <SentMessage sentMessages={sentMessages} />
-        <ReceivedMessage receivedMessages={receivedMessages} />
-      </ScrollView>
       <View style={styles.InputContainer}>
         <MessageInput />
       </View>
