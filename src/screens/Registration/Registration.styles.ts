f3f6d7991569ccbd0e2eeff3f539e0c9650e96f8
NikithaMedikonda <nikitha.medikonda@everest.engineering>
2025-05-20 13:06:13 +0530

    Adjust inline errors

--- src/screens/Registration/Registration.styles.ts
+++ src/screens/Registration/Registration.styles.ts
@@ -37,7 +37,7 @@ export const getStyles = (colors: Colors) =>
       fontSize: 15,
       alignSelf: 'flex-start',
       width: width * 0.75,
-      marginLeft: width * 0.011,
+      marginLeft: width * 0.027,
     },
     phoneErrorText: {
       color: 'red',
