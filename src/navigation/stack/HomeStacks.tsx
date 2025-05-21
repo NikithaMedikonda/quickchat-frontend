170a85b9c8d25965c39b19328a511911e8cc149d
AnushaUppuu <anusha.uppu@everest.engineering>
2025-05-21 12:37:36 +0530

    Add the individual chat screen

--- src/navigation/stack/HomeStacks.tsx
+++ src/navigation/stack/HomeStacks.tsx
@@ -1,9 +1,11 @@
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
 import {ContactsDisplay} from '../../screens/ContactsDisplay/ContactsDisplay';
 import {Home} from '../../screens/Home/Home';
+import {IndividualChat} from '../../screens/IndividualChat/IndividualChat';
+import {HomeStackParamList} from '../../types/usenavigation.type';
 
-const Stack = createNativeStackNavigator();
-
+const Stack = createNativeStackNavigator<HomeStackParamList>();
 export const HomeStacks = () => {
   return (
     <Stack.Navigator initialRouteName={'home'}>
@@ -11,6 +13,11 @@ export const HomeStacks = () => {
         name="home"
         component={Home}
         options={{headerShown: true}}
+      />
+      <Stack.Screen
+        name="individualChat"
+        component={IndividualChat}
+        options={{headerShown: false}}
       />
       <Stack.Screen
         name="contacts"
