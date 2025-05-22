2984cfd33cb829d0cb838086d1a61a2683af22d7
AnushaUppuu <anusha.uppu@everest.engineering>
2025-05-22 17:18:28 +0530

    Update the test cases, add test case check the navigation

--- src/components/IndividualChatHeader/IndividualChatHeader.test.tsx
+++ src/components/IndividualChatHeader/IndividualChatHeader.test.tsx
@@ -1,19 +1,28 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { render, screen } from '@testing-library/react-native';
+import {
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+} from '@testing-library/react-native';
 
-import { IndividualChatHeader } from './IndividualChatHeader';
-
+import {IndividualChatHeader} from './IndividualChatHeader';
+const mockNavigate = jest.fn();
+const mockGoBack = jest.fn();
+jest.mock('@react-navigation/native', () => ({
+  useNavigation: () => ({
+    navigate: mockNavigate,
+    setOptions: mockNavigate,
+    goBack: mockGoBack,
+  }),
+}));
 describe('IndividualChatHeader', () => {
   const userDetails = {
     name: 'Chitty',
     profilePicture: '../../assets/user.png',
+    phoneNumber: '',
   };
   beforeEach(() => {
-    render(
-      <NavigationContainer>
-        <IndividualChatHeader {...userDetails} />
-      </NavigationContainer>,
-    );
+    render(<IndividualChatHeader {...userDetails} />);
   });
   test('Should render the back arrow icon', () => {
     const backArrow = screen.getByA11yHint('back-arrow-icon');
@@ -34,5 +43,12 @@ describe('IndividualChatHeader', () => {
   test('Should render the more options icon', () => {
     const moreOptions = screen.getByA11yHint('more-options-icon');
     expect(moreOptions).toBeTruthy();
+  });
+  test('Should go back upon clicking the back-arrow', async () => {
+    const backArrow = screen.getByA11yHint('back-arrow-icon');
+    await waitFor(() => {
+      fireEvent.press(backArrow);
+    });
+    expect(mockGoBack).toHaveBeenCalledTimes(1);
   });
 });
