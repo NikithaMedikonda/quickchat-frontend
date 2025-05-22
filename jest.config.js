f386c7e5d9bb0cbe1d063310c224df4d1a951df6
NikithaMedikonda <nikitha.medikonda@everest.engineering>
2025-05-22 16:25:44 +0530

    Add react-native-libsodium in config file

--- jest.config.js
+++ jest.config.js
@@ -4,11 +4,11 @@ module.exports = {
     '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
   },
   transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native|react-navigation|@react-navigation|react-native-splash-screen|react-router-native|react-redux)/)',
+    'node_modules/(?!(react-native|@react-native|react-navigation|@react-navigation|react-native-splash-screen|react-router-native|react-redux|react-native-libsodium)/)',
   ],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   moduleNameMapper: {
     '/.(png|jpg|jpeg|svg)$/': '<rootDir>/__mocks__/fileMock.js',
   },
-   setupFiles: ['<rootDir>/jest.global-mocks.ts'],
+  setupFiles: ['<rootDir>/jest.global-mocks.ts'],
 };
