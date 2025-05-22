31f6fecb17a11a2a7e279cc4a22d19a9ed0c62ec
NikithaMedikonda <nikitha.medikonda@everest.engineering>
2025-05-22 16:27:11 +0530

    Modify generation of keys with react-native-libsodium library

--- src/services/__tests__/KeyGeneration.test.ts
+++ src/services/__tests__/KeyGeneration.test.ts
@@ -1,19 +1,23 @@
-import Rsa from 'react-native-rsa';
+import { crypto_box_keypair, to_base64 } from 'react-native-libsodium';
 import { keyGeneration } from '../keyGeneration';
 
-
-jest.mock('react-native-rsa', () => {
-  return jest.fn().mockImplementation(() => ({
-    generateKeys: jest.fn().mockResolvedValue({
-      public: 'mockPublicKey',
-      private: 'mockPrivateKey',
-    }),
-  }));
-});
+jest.mock('react-native-libsodium', () => ({
+  crypto_box_keypair: jest.fn(),
+  to_base64: jest.fn(),
+}));
 
 describe('KeyGeneration', () => {
   it('should generate and return public and private keys', async () => {
+    (crypto_box_keypair as jest.Mock).mockResolvedValue({
+      publicKey: new Uint8Array([1, 2, 3]),
+      privateKey: new Uint8Array([4, 5, 6]),
+    });
+
+    (to_base64 as jest.Mock).mockResolvedValueOnce('mockPublicKey');
+    (to_base64 as jest.Mock).mockResolvedValueOnce('mockPrivateKey');
+
     const keys = await keyGeneration();
+
     expect(keys).toEqual({
       publicKey: 'mockPublicKey',
       privateKey: 'mockPrivateKey',
@@ -21,11 +25,10 @@ describe('KeyGeneration', () => {
   });
 
   it('should handle errors during key generation', async () => {
-    (Rsa as jest.Mock).mockImplementation(() => {
+    (crypto_box_keypair as jest.Mock).mockImplementation(() => {
       throw new Error('Error occured while generating Keys');
     });
 
-    await expect(
-      keyGeneration()).rejects.toThrow('Error occured while generating Keys');
+    await expect(keyGeneration()).rejects.toThrow('Error occured while generating Keys');
   });
 });
