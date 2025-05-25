991cdeccfed76400bf7194dcd9d85138b5d76d27
goldie <goldie@Anoosha-Sanugula.local>
2025-05-25 15:43:10 +0530

    Add images in the themes

--- src/themes/images.ts
+++ src/themes/images.ts
@@ -1,13 +1,15 @@
-import { ImageSourcePropType} from 'react-native';
+import {ImageSourcePropType} from 'react-native';
+import AndroidBackArrowDark from '../assets/AndroidBackArrowDark.png';
+import BinDark from '../assets/BinDark.png';
+import HomeAddDark from '../assets/HomeAddDark.png';
+import IOSBackArrowDark from '../assets/IOSBackArrowDark.png';
+import LogoutDark from '../assets/LogoutDark.png';
+import PencilDark from '../assets/PencilDark.png';
+import ProfileDotsDark from '../assets/ProfileDotsDark.png';
 import QuickChatDarkLogo from '../assets/QuickChatDarkLogo.png';
 import TabMessageDark from '../assets/TabMessageDark.png';
-import TabUnreadDark from '../assets/TabUnreadDark.png';
 import TabProfileDark from '../assets/TabProfileDark.png';
-import HomeAddDark from '../assets/HomeAddDark.png';
-import ProfileDotsDark from '../assets/ProfileDotsDark.png';
-import BinDark from '../assets/BinDark.png';
-import PencilDark from '../assets/PencilDark.png';
-import LogoutDark from '../assets/LogoutDark.png';
+import TabUnreadDark from '../assets/TabUnreadDark.png';
 import chatblockDark from '../assets/chatblockDark.png';
 
 export const useImagesColors = (): {
@@ -21,17 +23,21 @@ export const useImagesColors = (): {
   pencil: ImageSourcePropType;
   logoutImage: ImageSourcePropType;
   chatblockImage: ImageSourcePropType;
+  androidBackArrow: ImageSourcePropType;
+  iOSBackArrow: ImageSourcePropType;
 } => {
   return {
-    logo:  QuickChatDarkLogo ,
-    tabHome:  TabMessageDark ,
-    tabUnread:  TabUnreadDark,
-    tabProfile:  TabProfileDark ,
-    homeAdd:   HomeAddDark,
-    profileDots:  ProfileDotsDark ,
-    bin:  BinDark ,
-    pencil:  PencilDark ,
-    logoutImage:  LogoutDark,
-    chatblockImage : chatblockDark,
+    logo: QuickChatDarkLogo,
+    tabHome: TabMessageDark,
+    tabUnread: TabUnreadDark,
+    tabProfile: TabProfileDark,
+    homeAdd: HomeAddDark,
+    profileDots: ProfileDotsDark,
+    bin: BinDark,
+    pencil: PencilDark,
+    logoutImage: LogoutDark,
+    chatblockImage: chatblockDark,
+    androidBackArrow: AndroidBackArrowDark,
+    iOSBackArrow: IOSBackArrowDark,
   };
 };
