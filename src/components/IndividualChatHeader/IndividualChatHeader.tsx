3290ddfcb10cc028ec5e77a6625259980d7a98ca
goldie <goldie@Anoosha-Sanugula.local>
2025-05-25 15:38:15 +0530

    Modify alignments of profile picture, back navigation and name

--- src/components/IndividualChatHeader/IndividualChatHeader.tsx
+++ src/components/IndividualChatHeader/IndividualChatHeader.tsx
@@ -1,39 +1,40 @@
-import {Image, Platform, Text, TouchableOpacity, View} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
+import {Image, Platform, Text, TouchableOpacity, View} from 'react-native';
 
 import {useThemeColors} from '../../themes/colors';
-import {individualChatHeaderStyles} from './IndividualChatHeader.styles';
+import {useImagesColors} from '../../themes/images';
 import {HomeStackProps} from '../../types/usenavigation.type';
 import {UserDetails} from '../../types/user.types';
+import {individualChatHeaderStyles} from './IndividualChatHeader.styles';
 
 export const IndividualChatHeader = ({name, profilePicture}: UserDetails) => {
   const colors = useThemeColors();
+  const {androidBackArrow, iOSBackArrow} = useImagesColors();
   const navigation = useNavigation<HomeStackProps>();
   const styles = individualChatHeaderStyles(colors);
   return (
     <View style={styles.content}>
-      <TouchableOpacity
-        onPress={() => {
-          navigation.goBack();
-        }}>
+      <View style={styles.container}>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.goBack();
+          }}>
+          <Image
+            source={Platform.OS === 'android' ? androidBackArrow : iOSBackArrow}
+            accessibilityHint="back-arrow-icon"
+            style={styles.backArrow}
+          />
+        </TouchableOpacity>
         <Image
-          source={
-            Platform.OS === 'ios'
-              ? require('../../assets/iOS-back-arrow.png')
-              : require('../../assets/android-back-arrow.png')
-          }
-          accessibilityHint="back-arrow-icon"
-          style={styles.backArrow}
+          source={{uri: profilePicture}}
+          style={styles.profilePicture}
+          accessibilityHint="profile-picture"
         />
-      </TouchableOpacity>
-      <Image
-        source={{uri: profilePicture}}
-        style={styles.profilePicture}
-        accessibilityHint="profile-picture"
-      />
-      <Text style={styles.username} accessibilityHint="username-text">
-        {name}
-      </Text>
+        <Text style={styles.username} accessibilityHint="username-text">
+          {name}
+        </Text>
+      </View>
+
       <Image
         source={require('../../assets/more-options-icon.png')}
         style={styles.moreOptionsIcon}
