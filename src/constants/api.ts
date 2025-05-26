41f30d82a19b6e155b704123c15e4f33fe1b3fce
vinaysaichelupuri <vinaysai.chelupuri@everest.engineering>
2025-05-26 15:00:38 +0530

    Update Render URL

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,4 +1,1 @@
-import { Platform } from "react-native";
-
 export const API_URL = 'https://quickchat-backend-nvfc.onrender.com';
-// export const API_URL = Platform.OS === 'ios' ? `http://localhost:5050` : `http://10.0.2.2:5050`;
