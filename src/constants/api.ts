f8518b0ce1d8480b2d4b8456e947488ac77e9507
vinaysaichelupuri <vinaysai.chelupuri@everest.engineering>
2025-05-26 14:59:21 +0530

    Update API constants in api.ts

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,1 +1,4 @@
+import { Platform } from "react-native";
+
 export const API_URL = 'https://quickchat-backend-nvfc.onrender.com';
+// export const API_URL = Platform.OS === 'ios' ? `http://localhost:5050` : `http://10.0.2.2:5050`;
