be39a4f9e6370d37dbdf05cc1b46929b500dd282
vinaysaichelupuri <vinaysai.chelupuri@everest.engineering>
2025-05-26 14:59:08 +0530

    Refactor and update IndividualChatHeader.tsx

--- src/components/IndividualChatHeader/IndividualChatHeader.tsx
+++ src/components/IndividualChatHeader/IndividualChatHeader.tsx
@@ -1,17 +1,26 @@
 import {useNavigation} from '@react-navigation/native';
 import {Image, Platform, Text, TouchableOpacity, View} from 'react-native';
-
 import {useThemeColors} from '../../themes/colors';
 import {useImagesColors} from '../../themes/images';
 import {HomeStackProps} from '../../types/usenavigation.type';
 import {UserDetails} from '../../types/user.types';
+import {ChatOptionsModal} from '../ChatOptionsModal/ChatOptionsModal';
 import {individualChatHeaderStyles} from './IndividualChatHeader.styles';
+import {useState} from 'react';
 
-export const IndividualChatHeader = ({name, profilePicture}: UserDetails) => {
+export const IndividualChatHeader = ({name, profilePicture,isBlocked}: UserDetails) => {
+  const [modalVisible, setModalVisible] = useState(false);
   const colors = useThemeColors();
+  const {profileDots} = useImagesColors();
   const {androidBackArrow, iOSBackArrow} = useImagesColors();
   const navigation = useNavigation<HomeStackProps>();
   const styles = individualChatHeaderStyles(colors);
+  const modelVisible = () => {
+    setModalVisible(true);
+  };
+  const onClose = () => {
+    setModalVisible(false);
+  };
   return (
     <View style={styles.content}>
       <View style={styles.container}>
@@ -34,12 +43,14 @@ export const IndividualChatHeader = ({name, profilePicture}: UserDetails) => {
           {name}
         </Text>
       </View>
-
-      <Image
-        source={require('../../assets/more-options-icon.png')}
-        style={styles.moreOptionsIcon}
-        accessibilityHint="more-options-icon"
-      />
+      <TouchableOpacity onPress={modelVisible}>
+        <Image
+          source={profileDots}
+          style={styles.moreOptionsIcon}
+          accessibilityHint="more-options-icon"
+        />
+      </TouchableOpacity>
+      <ChatOptionsModal visible={modalVisible} onClose={onClose} isBlocked={isBlocked}/>
     </View>
   );
 };
