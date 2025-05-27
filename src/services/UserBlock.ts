410a704f8adecf6961707fab28b3641bdc4ce6c2
vinaysaichelupuri <vinaysai.chelupuri@everest.engineering>
2025-05-27 10:29:07 +0530

    Add UserBlock service

---
+++ src/services/UserBlock.ts
@@ -0,0 +1,32 @@
+import {API_URL} from '../constants/api';
+
+export const blockUser = async ({
+  blockerPhoneNumber,
+  blockedPhoneNumber,
+  authToken,
+}: {
+  blockerPhoneNumber: string;
+  blockedPhoneNumber: string;
+  authToken: string;
+}) => {
+  console.log('Api hitted');
+  console.log(blockedPhoneNumber);
+  console.log(blockerPhoneNumber);
+  const response = await fetch(`${API_URL}/api/block/users`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      authorization: `Bearer ${authToken}`,
+    },
+    body: JSON.stringify({
+      blockerPhoneNumber: blockerPhoneNumber,
+      blockedPhoneNumber: blockedPhoneNumber,
+    }),
+  });
+  const data = await response.json();
+  console.log(response.status);
+  return {
+    status: response.status,
+    data,
+  };
+};
