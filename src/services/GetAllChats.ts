ad28913ce6c01bf8e82b7e69a062ed3ce769c199
NikithaMedikonda <nikitha.medikonda@everest.engineering>
2025-05-28 09:53:00 +0530

    Handle case with no authToken

--- src/services/GetAllChats.ts
+++ src/services/GetAllChats.ts
@@ -1,20 +1,20 @@
 import EncryptedStorage from 'react-native-encrypted-storage';
-import {API_URL} from '../constants/api';
+import { API_URL } from '../constants/api';
 
 export const getAllChats = async () => {
   try {
     const authToken = await EncryptedStorage.getItem('authToken');
-    if (!authToken) {
-      throw new Error('Authorization failed');
+    if(!authToken) {
+      return {status : 401};
     }
 
     const userDetails = await EncryptedStorage.getItem('user');
-
-    let userPhoneNumber;
-    if (userDetails) {
-      const userDetailsParsed = JSON.parse(userDetails);
-      userPhoneNumber = userDetailsParsed.phoneNumber;
+    if (!userDetails) {
+      return {data: null};
     }
+
+    const parsedUserDetails = JSON.parse(userDetails);
+    const userPhoneNumber = parsedUserDetails.phoneNumber;
 
     const response = await fetch(`${API_URL}/api/chats/user`, {
       method: 'POST',
@@ -24,12 +24,17 @@ export const getAllChats = async () => {
       },
       body: JSON.stringify({userPhoneNumber}),
     });
+
     const data = await response.json();
+
     return {
       status: response.status,
       data,
     };
-  } catch (error) {
-    throw new Error(`Error while fetching chats ${(error as Error).message}`);
+  } catch {
+    return {
+      status: 500,
+      data: {message: 'Internal error fetching chats.'},
+    };
   }
 };
