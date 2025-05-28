b8e8aaede3e69a17bc75d2d893a98e9c85ab3400
NikithaMedikonda <nikitha.medikonda@everest.engineering>
2025-05-28 09:54:02 +0530

    Handle case: No authToken

--- src/screens/AllChats/AllChats.tsx
+++ src/screens/AllChats/AllChats.tsx
@@ -1,24 +1,26 @@
-import {useNavigation} from '@react-navigation/native';
-import { useEffect, useLayoutEffect, useState} from 'react';
-import {ScrollView, TouchableOpacity, View} from 'react-native';
-import {useDispatch} from 'react-redux';
-import {useThemeColors} from '../../themes/colors';
-import {HomeStackProps, HomeTabsProps} from '../../types/usenavigation.type';
+import { useNavigation } from '@react-navigation/native';
+import { useEffect, useLayoutEffect, useState } from 'react';
+import { ScrollView, TouchableOpacity, View } from 'react-native';
+import { useDispatch } from 'react-redux';
+import { useThemeColors } from '../../themes/colors';
+import { HomeStackProps, NavigationProps } from '../../types/usenavigation.type';
 
-import {ChatBox} from '../../components/ChatBox/ChatBox';
-import {PlusIcon} from '../../components/PlusIcon/PlusIcon';
-import {nameNumberIndex} from '../../helpers/nameNumberIndex';
-import {normalise} from '../../helpers/normalisePhoneNumber';
-import {getAllChats} from '../../services/GetAllChats';
-import {hide, show} from '../../store/slices/loadingSlice';
+import EncryptedStorage from 'react-native-encrypted-storage';
+import { ChatBox } from '../../components/ChatBox/ChatBox';
+import { PlusIcon } from '../../components/PlusIcon/PlusIcon';
+import { numberNameIndex } from '../../helpers/nameNumberIndex';
+import { normalise } from '../../helpers/normalisePhoneNumber';
+import { getAllChats } from '../../services/GetAllChats';
+import { hide, show } from '../../store/slices/loadingSlice';
+import { logout } from '../../store/slices/loginSlice';
 import {
   setAlertMessage,
   setAlertTitle,
   setAlertType,
   setAlertVisible,
 } from '../../store/slices/registrationSlice';
-import {Home} from '../Home/Home';
-import {getStyles} from './AllChats.styles';
+import { Home } from '../Home/Home';
+import { getStyles } from './AllChats.styles';
 
 export interface Chat {
   chatId: string;
@@ -32,15 +34,18 @@ export interface Chat {
   unreadCount: number;
 }
 
+type ContactNameMap = Record<string, string>;
+
 export const AllChats = () => {
-  const navigation = useNavigation<HomeTabsProps>();
+  const navigation = useNavigation<NavigationProps>();
+
   const homeStackNavigation = useNavigation<HomeStackProps>();
   const dispatch = useDispatch();
   const colors = useThemeColors();
   const styles = getStyles(colors);
 
   const [chats, setChats] = useState<Chat[]>([]);
-  const [contactName, setContactName] = useState<Record<string, string>>({});
+  const [contactNameMap, setContactNameMap] = useState<ContactNameMap>({});
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -64,28 +69,44 @@ export const AllChats = () => {
     };
 
     const fetchChats = async () => {
-      try {
-        const phoneName = await nameNumberIndex();
-        if (phoneName) {
-          setContactName(phoneName);
-        }
-        dispatch(show());
-        const response = await getAllChats();
-        if (response.status === 200) {
-          setChats(response.data.chats);
-        }
+      dispatch(show());
+
+      const phoneNameMap = await numberNameIndex();
+      if (phoneNameMap === null) {
         dispatch(hide());
-      } catch (error) {
-        dispatch(hide());
-        showAlert(
-          'info',
-          'Network Connection',
-          'Unable to fetch chats at this moment.',
-        );
+        dispatch(logout());
+        showAlert('error', 'Session Expired', 'Please login again.');
+        await EncryptedStorage.clear();
+        setTimeout(() => {
+          dispatch(setAlertVisible(false));
+          navigation.replace('login');
+        }, 1000);
+        return;
       }
+
+      setContactNameMap(phoneNameMap as ContactNameMap);
+
+      const response = await getAllChats();
+
+      if (response.status === 401 || response.data === null) {
+        dispatch(hide());
+        dispatch(logout());
+        showAlert('error', 'Session Expired', 'Please login again.');
+        await EncryptedStorage.clear();
+        setTimeout(() => {
+          dispatch(setAlertVisible(false));
+          navigation.replace('login');
+        }, 1000);
+      } else if (response.status === 200 && response.data) {
+        setChats(response.data.chats);
+      } else {
+        showAlert('info', 'Unable to Fetch Chats', 'Please try again later.');
+      }
+      dispatch(hide());
     };
+
     fetchChats();
-  }, [dispatch]);
+  }, [dispatch, navigation]);
 
   if (chats.length === 0) {
     return <Home />;
@@ -101,7 +122,7 @@ export const AllChats = () => {
               homeStackNavigation.navigate('individualChat', {
                 user: {
                   name:
-                    contactName[normalise(chat.phoneNumber)] ||
+                    contactNameMap[normalise(chat.phoneNumber)] ||
                     chat.phoneNumber,
                   profilePicture: chat.contactProfilePic,
                   phoneNumber: chat.phoneNumber,
@@ -111,7 +132,7 @@ export const AllChats = () => {
             <ChatBox
               image={chat.contactProfilePic}
               name={
-                contactName[normalise(chat.phoneNumber)] || chat.phoneNumber
+                contactNameMap[normalise(chat.phoneNumber)] || chat.phoneNumber
               }
               description={chat.lastMessageText}
               timestamp={chat.lastMessageTimestamp}
