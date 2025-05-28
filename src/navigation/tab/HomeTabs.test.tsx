1655c8bf42b451be780d1fb91e6bc61b91e9e217
NikithaMedikonda <nikitha.medikonda@everest.engineering>
2025-05-28 09:52:19 +0530

    Mock getItem with user in encrypted-storage

--- src/navigation/tab/HomeTabs.test.tsx
+++ src/navigation/tab/HomeTabs.test.tsx
@@ -1,17 +1,22 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { render } from '@testing-library/react-native';
-import { Provider } from 'react-redux';
-import { store } from '../../store/store';
-import { HomeTabs } from './HomeTabs';
+import {NavigationContainer} from '@react-navigation/native';
+import {render, waitFor} from '@testing-library/react-native';
+import {Provider} from 'react-redux';
+import {store} from '../../store/store';
+import {HomeTabs} from './HomeTabs';
 
 jest.mock('react-native-encrypted-storage', () => ({
-  getItem: jest.fn(),
+  getItem: jest
+    .fn()
+    .mockResolvedValue(
+      JSON.stringify({name: 'Mamatha', phoneNumber: '+91 6303974914'}),
+    ),
   setItem: jest.fn(),
   clear: jest.fn(),
 }));
 
 jest.mock('react-native-contacts', () => ({
   getContactsByPhoneNumber: jest.fn(),
+  getAllWithoutPhotos: jest.fn(),
 }));
 
 jest.mock('react-native-image-crop-picker', () => ({
@@ -27,14 +32,22 @@ jest.mock('../../permissions/ImagePermissions', () => ({
   requestPermissions: jest.fn().mockResolvedValue(false),
 }));
 
+jest.mock('../../helpers/nameNumberIndex', () => ({
+  numberNameIndex: jest.fn().mockResolvedValue({
+    '+916303961097': 'Test User',
+  }),
+}));
+
 describe('Welcome Screen', () => {
-  it('renders the Tabs', () => {
-    render(
-      <Provider store={store}>
-        <NavigationContainer>
-          <HomeTabs />
-        </NavigationContainer>
-      </Provider>
-    );
+  it('renders the Tabs', async () => {
+    await waitFor(() => {
+      render(
+        <Provider store={store}>
+          <NavigationContainer>
+            <HomeTabs />
+          </NavigationContainer>
+        </Provider>,
+      );
+    });
   });
 });
