687cde4b62cd054c988ef6f17e8c837dabbc6c21
AnushaUppuu <anusha.uppu@everest.engineering>
2025-05-28 15:25:00 +0530

    Create the receiveJoined, receiveOnline and receiveOffline socket functions

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -1,8 +1,7 @@
 import io from 'socket.io-client';
-import {SentPrivateMessage} from '../types/messsage.types';
 import { API_URL } from '../constants/api';
+import { SentPrivateMessage } from '../types/messsage.types';
 export const newSocket = io(`${API_URL}`);
-
 export async function socketConnection(userPhoneNumber: string) {
   newSocket.emit('join', userPhoneNumber);
 }
@@ -10,15 +9,29 @@ export async function receivePrivateMessage(
   senderPhoneNumber: string,
   callback: (message: SentPrivateMessage) => void,
 ) {
-  let result: SentPrivateMessage = {
-    recipientPhoneNumber: '',
-    message: '',
-    senderPhoneNumber: '',
-    timestamp: '',
-    status: '',
-  };
   await newSocket.on(`receive_private_message_${senderPhoneNumber}`, callback);
-  return result;
+}
+export async function receiveOnline({
+  withChattingNumber,
+  setIsOnline,
+}: {
+  withChattingNumber: string;
+  setIsOnline: (isOnline: boolean) => void;
+}) {
+  await newSocket.on(`isOnline_with_${withChattingNumber}`, (data: any) => {
+    setIsOnline(data.isOnline);
+  });
+}
+export async function receiveOffline({
+  withChattingNumber,
+  setIsOnline,
+}: {
+  withChattingNumber: string;
+  setIsOnline: (message: boolean) => void;
+}) {
+  await newSocket.on(`offline_with_${withChattingNumber}`, (data: any) => {
+    setIsOnline(data.online);
+  });
 }
 export async function sendPrivateMessage(payload: SentPrivateMessage) {
   newSocket.emit('send_private_message', {
@@ -26,5 +39,19 @@ export async function sendPrivateMessage(payload: SentPrivateMessage) {
     message: payload.message,
     senderPhoneNumber: payload.senderPhoneNumber,
     timestamp: payload.timestamp,
+  });
+}
+export async function receiveJoined({
+  userPhoneNumber,
+  setSocketId,
+}: {
+  userPhoneNumber: string;
+  setSocketId: (si: string) => void;
+}) {
+  await newSocket.on('I-joined', (data: any) => {
+    console.log('In I joined', data.phoneNumber);
+    if (data.phoneNumber === userPhoneNumber) {
+      setSocketId(data.socketId);
+    }
   });
 }
