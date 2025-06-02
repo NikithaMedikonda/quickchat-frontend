d16ad75d875bf8571e4eb8fb8ddadbb457c08a10
vinaysaichelupuri <vinaysai.chelupuri@everest.engineering>
2025-06-02 11:56:48 +0530

    Refactor Unread messages screen

--- src/screens/Unread/Unread.tsx
+++ src/screens/Unread/Unread.tsx
@@ -1,6 +1,7 @@
 import {Text, View} from 'react-native';
-
+import { useDeviceCheck } from '../../services/useDeviceCheck';
 export const Unread = () => {
+  useDeviceCheck();
   return (
     <View>
       <Text>Hello unread</Text>
