32fc09cdb76e193c0a37dda591cfa0d2e6f9ae91
NikithaMedikonda <nikitha.medikonda@everest.engineering>
2025-06-02 12:18:49 +0530

    Update tests to handle showfullTime

--- src/components/TimeStamp/TimeStamp.test.tsx
+++ src/components/TimeStamp/TimeStamp.test.tsx
@@ -13,23 +13,65 @@ afterAll(() => {
 
 describe('TimeStamp component', () => {
   test(' should display correct time for today', () => {
-    const {getByText} = render(<TimeStamp messageTime="2025-05-13T10:00:00" isSent= {true}/>);
+    const {getByText} = render(
+      <TimeStamp messageTime="2025-05-13T10:00:00" isSent={true} />,
+    );
     expect(getByText(/10:00 AM/i)).toBeTruthy();
   });
 
   test('Should display "Yesterday" if the time is more than one day', () => {
-    const {getByText} = render(<TimeStamp messageTime="2025-05-12T08:00:00" isSent= {true}/>);
+    const {getByText} = render(
+      <TimeStamp messageTime="2025-05-12T08:00:00" isSent={true} />,
+    );
     expect(getByText(/Yesterday/i)).toBeTruthy();
   });
 
   test('Should display day name within last 7 days', () => {
     const dayName = moment('2025-05-09').format('dddd');
-    const {getByText} = render(<TimeStamp messageTime="2025-05-09T09:00:00" isSent= {true}/>);
-     expect(getByText(`${dayName}`)).toBeTruthy();
+    const {getByText} = render(
+      <TimeStamp messageTime="2025-05-09T09:00:00" isSent={true} />,
+    );
+    expect(getByText(`${dayName}`)).toBeTruthy();
   });
 
   test('Should display full date, if the timestamps are older than 7 days', () => {
-    const {getByText} = render(<TimeStamp messageTime="2025-04-30T15:30:00"isSent= {true} />);
+    const {getByText} = render(
+      <TimeStamp messageTime="2025-04-30T15:30:00" isSent={true} />,
+    );
     expect(getByText('Apr 30, 2025')).toBeTruthy();
+  });
+
+  test('Should display only time for today with showFullTime', () => {
+    const {getByText} = render(
+      <TimeStamp
+        messageTime="2025-05-13T09:45:00"
+        isSent={true}
+        showFullTime
+      />,
+    );
+    expect(getByText('9:45 AM')).toBeTruthy();
+  });
+
+  test('Should display "Yesterday, time" when showFullTime is true', () => {
+    const {getByText} = render(
+      <TimeStamp
+        messageTime="2025-05-12T08:00:00"
+        isSent={true}
+        showFullTime
+      />,
+    );
+    expect(getByText('Yesterday, 8:00 AM')).toBeTruthy();
+  });
+
+  test('Should display "Day, time" within last 7 days when showFullTime is true', () => {
+    const fullTime = moment('2025-05-09T09:00:00').format('dddd, h:mm A');
+    const {getByText} = render(
+      <TimeStamp
+        messageTime="2025-05-09T09:00:00"
+        isSent={true}
+        showFullTime
+      />,
+    );
+    expect(getByText(fullTime)).toBeTruthy();
   });
 });
