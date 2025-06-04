d26db8566c765b1d36d22f80d32fb09423cad18f
ushasri2645 <ugudikandula@everest.engineering>
2025-06-04 10:16:01 +0530

    Re order the import statements.

--- App.tsx
+++ App.tsx
@@ -1,15 +1,15 @@
-import './src/i18n/i18n.config';
-import {useEffect} from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
+import { useEffect } from 'react';
 import * as RNLocalize from 'react-native-localize';
 import SplashScreen from 'react-native-splash-screen';
-import {i18next} from './src/i18n/i18n.config';
-import {InitialStacks} from './src/navigation/stack/InitialStacks';
-import {LoadingComponent} from './src/components/Loading/Loading';
-import {Provider} from 'react-redux';
-import {store} from './src/store/store';
-import {getDBInstance} from './src/database/connection/connection';
-import {createTables} from './src/database/models/schema';
+import { Provider } from 'react-redux';
+import { LoadingComponent } from './src/components/Loading/Loading';
+import { getDBInstance } from './src/database/connection/connection';
+import { createTables } from './src/database/models/schema';
+import './src/i18n/i18n.config';
+import { i18next } from './src/i18n/i18n.config';
+import { InitialStacks } from './src/navigation/stack/InitialStacks';
+import { store } from './src/store/store';
 
 export const App = () => {
   useEffect(() => {
