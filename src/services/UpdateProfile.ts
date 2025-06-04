06d2cdab4c0f5bbf9669982ae61300befcd6ba89
anoosha-sanugula <goldie@Anoosha-Sanugula.local>
2025-06-04 16:40:19 +0530

    Trim the user fields while edit profile

--- src/services/UpdateProfile.ts
+++ src/services/UpdateProfile.ts
@@ -3,7 +3,7 @@ import {API_URL} from '../constants/api';
 export const updateProfile = async (
   editProfileForm: {
     phoneNumber: string;
-    image?: string;
+    image: string;
     firstName: string;
     lastName: string;
     email?: string;
@@ -11,29 +11,35 @@ export const updateProfile = async (
   },
   user: {
     phoneNumber: string;
-    image?: string;
+    profilePicture: string;
     firstName: string;
     lastName: string;
     email?: string;
   },
 ) => {
-  const userData: any = {};
+  const userData: {
+    phoneNumber?: string;
+    profilePicture?: string;
+    firstName?: string;
+    lastName?: string;
+    email?: string | null;
+  } = {};
   if (user.phoneNumber === editProfileForm.phoneNumber) {
     userData.phoneNumber = editProfileForm.phoneNumber;
   }
   if (user.firstName !== editProfileForm.firstName) {
-    userData.firstName = editProfileForm.firstName;
+    userData.firstName = editProfileForm.firstName.trim();
   }
 
   if (user.lastName !== editProfileForm.lastName) {
-    userData.lastName = editProfileForm.lastName;
+    userData.lastName = editProfileForm.lastName.trim();
   }
 
-  if (editProfileForm.email && user.email !== editProfileForm.email) {
-    userData.email = editProfileForm.email;
+  if (user.email !== editProfileForm.email) {
+    userData.email = editProfileForm.email?.trim() || null;
   }
 
-  if (editProfileForm.image && user.image !== editProfileForm.image) {
+  if (user.profilePicture !== editProfileForm.image) {
     userData.profilePicture = editProfileForm.image;
   }
   const response = await fetch(`${API_URL}/api/user`, {
