0496809840858925a69c5306c2e0432d7d17edc5
vinaysaichelupuri <vinaysai.chelupuri@everest.engineering>
2025-06-05 10:09:26 +0530

    Include FCM token logic in LoginUser service

--- src/services/LoginUser.ts
+++ src/services/LoginUser.ts
@@ -1,12 +1,15 @@
 import {API_URL} from '../constants/api';
+import { getFCMToken } from '../permissions/NotificationPermissions';
 
 export const loginUser = async (
   form: {phoneNumber: string; password: string},
   deviceId: string,
 ) => {
+  const fcmToken = await getFCMToken();
   const userData = {
     ...form,
     deviceId,
+    fcmToken,
   };
 
   const response = await fetch(`${API_URL}/api/user`, {
