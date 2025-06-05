d7e9cb51f4a612060516f5709241b3e1d405758c
vinaysaichelupuri <vinaysai.chelupuri@everest.engineering>
2025-06-05 10:09:26 +0530

    Update App.tsx for push notification integration

--- App.tsx
+++ App.tsx
@@ -1,19 +1,24 @@
 import './src/i18n/i18n.config';
-import {useEffect} from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+import { useEffect } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import * as RNLocalize from 'react-native-localize';
 import SplashScreen from 'react-native-splash-screen';
-import {i18next} from './src/i18n/i18n.config';
-import {InitialStacks} from './src/navigation/stack/InitialStacks';
-import {LoadingComponent} from './src/components/Loading/Loading';
-import {Provider} from 'react-redux';
-import {store} from './src/store/store';
+import { i18next } from './src/i18n/i18n.config';
+import { InitialStacks } from './src/navigation/stack/InitialStacks';
+import { LoadingComponent } from './src/components/Loading/Loading';
+import { Provider } from 'react-redux';
+import { store } from './src/store/store';
+import { getFCMToken, listenForForegroundMessages } from './src/permissions/NotificationPermissions';
 
 export const App = () => {
   useEffect(() => {
     SplashScreen.hide();
     const languageCode = RNLocalize.getLocales()[0].languageCode;
     i18next.changeLanguage(languageCode);
+  }, []);
+    useEffect(() => {
+    getFCMToken();
+    listenForForegroundMessages();
   }, []);
 
   return (
