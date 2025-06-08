3a91775df33c68bb997b30c41988942514cec0d9
vinaysaichelupuri <vinaysai.chelupuri@everest.engineering>
2025-06-08 23:56:25 +0530

    Configure Firebase messaging service worker

--- firebase-messaging.js
+++ firebase-messaging.js
@@ -1,14 +1,26 @@
 import messaging from '@react-native-firebase/messaging';
 import notifee from '@notifee/react-native';
+import { numberNameIndex } from './src/helpers/nameNumberIndex';
+import { normalise } from './src/helpers/normalisePhoneNumber';
 
 messaging().setBackgroundMessageHandler(async remoteMessage => {
-  const { title, body } = remoteMessage.notification ?? {};
-  if (title && body) {
+  const title = remoteMessage.data?.title;
+  const body = remoteMessage.data?.body;
+
+  const index = await numberNameIndex();
+  if (title && body && index) {
+    const normalisedBody = normalise(body);
+    const name = index[normalisedBody] || body;
+
     await notifee.displayNotification({
-      title,
-      body,
+      title: 'Quick Chat',
+      body: `New Message from ${name}`,
       android: {
-        channelId: 'default',
+        channelId: 'quickchat',
+        smallIcon: 'ic_launcher',
+        pressAction: {
+          id: 'default',
+        },
       },
     });
   }
