d918e73ff9f1708a1f7a642647b54772687b198d
nagusha <nagusha.madasu@everest.engineering>
2025-06-10 11:36:31 +0530

    Add a order of imports

--- src/screens/Login/Login.styles.ts
+++ src/screens/Login/Login.styles.ts
@@ -1,5 +1,5 @@
-import {Dimensions, StyleSheet} from 'react-native';
-import {Colors} from '../../themes/colors';
+import { Dimensions, StyleSheet } from 'react-native';
+import { Colors } from '../../themes/colors';
 const {width, height} = Dimensions.get('window');
 
 export const loginStyles = (colors: Colors) =>
