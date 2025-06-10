d570cda86b4d6365fc8ce2b285268bdc11d0f028
nagusha <nagusha.madasu@everest.engineering>
2025-06-10 11:36:13 +0530

    Add a order of imports

--- src/screens/Welcome/Welcome.test.tsx
+++ src/screens/Welcome/Welcome.test.tsx
@@ -4,9 +4,9 @@ import {
   screen,
   waitFor,
 } from '@testing-library/react-native';
-import {Welcome} from './Welcome';
 import { Provider } from 'react-redux';
 import { store } from '../../store/store';
+import { Welcome } from './Welcome';
 
 const mockNavigate = jest.fn();
 jest.mock('@react-navigation/native', () => ({
@@ -23,9 +23,9 @@ describe('Welcome Screen', () => {
   it('renders the logo image', () => {
     render(
       <Provider store={store}>
-    <Welcome />
-    </Provider>
-  );
+        <Welcome />
+      </Provider>
+    );
     const image = screen.getByA11yHint('logo-image');
     expect(image.props.source).toEqual({
       testUri: '../../../src/assets/QuickChatLogo.png',
@@ -33,20 +33,20 @@ describe('Welcome Screen', () => {
   });
 
   it('renders the Get Started button', () => {
-    const {getByText} =   render(
+    const { getByText } = render(
       <Provider store={store}>
-    <Welcome />
-    </Provider>
-  );
+        <Welcome />
+      </Provider>
+    );
     expect(getByText('Get Started')).toBeTruthy();
   });
 
   it('navigates to register screen on button press', async () => {
-    const {getByText} =   render(
+    const { getByText } = render(
       <Provider store={store}>
-    <Welcome />
-    </Provider>
-  );
+        <Welcome />
+      </Provider>
+    );
     const button = getByText('Get Started');
     await waitFor(() => {
       fireEvent.press(button);
