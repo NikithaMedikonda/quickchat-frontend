c92512b57a924ce1819712cfe6f26f9429e1d91b
vinaysaichelupuri <vinaysai.chelupuri@everest.engineering>
2025-06-11 17:46:17 +0530

    Modified firebase-messaging.js

--- firebase-messaging.js
+++ firebase-messaging.js
@@ -1,23 +1,15 @@
 import messaging from '@react-native-firebase/messaging';
 import notifee from '@notifee/react-native';
-import { numberNameIndex } from './src/helpers/nameNumberIndex';
-import { normalise } from './src/helpers/normalisePhoneNumber';
 
 messaging().setBackgroundMessageHandler(async remoteMessage => {
-  const title = remoteMessage.data?.title;
-  const body = remoteMessage.data?.body;
-
-  const index = await numberNameIndex();
-  if (title && body && index) {
-    const normalisedBody = normalise(body);
-    const name = index[normalisedBody] || body;
-
+  const senderNumber = remoteMessage.data.senderPhoneNumber;
+  if (senderNumber) {
     await notifee.displayNotification({
       title: 'Quick Chat',
-      body: `New Message from ${name}`,
+      body: `New Message from ${senderNumber}`,
       android: {
         channelId: 'quickchat',
-        smallIcon: 'ic_launcher',
+        smallIcon: 'ic_stat_notification',
         pressAction: {
           id: 'default',
         },
