b68b8929b024b0a84149e84f14ca0245ed9592ec
ushasri2645 <ugudikandula@everest.engineering>
2025-06-12 16:40:02 +0530

    Update render url

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,4 +1,1 @@
-import { Platform } from "react-native";
-
-// export const API_URL = 'https://quickchat-backend-7pyd.onrender.com';
-export const API_URL = Platform.OS === 'ios' ? `http://localhost:5050` : `http://10.0.2.2:5050`;
+export const API_URL = 'https://quickchat-backend-7pyd.onrender.com';
