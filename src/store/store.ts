8e29188648345c46098186b0de0c705f1e277b6f
ushasri2645 <ugudikandula@everest.engineering>
2025-06-12 16:18:20 +0530

    Add chat slice

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,15 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { chatReducer } from './slices/chatSlice';
 import { loadingReducer } from './slices/loadingSlice';
 import { loginReducer } from './slices/loginSlice';
-import {registrationReducer} from './slices/registrationSlice';
+import { registrationReducer } from './slices/registrationSlice';
 import { unreadChatReducer } from './slices/unreadChatSlice';
 
 export const store = configureStore({
   reducer: {
     loading: loadingReducer,
     registration: registrationReducer,
-    login:loginReducer,
+    login: loginReducer,
     unread: unreadChatReducer,
+    chat: chatReducer,
   },
 });
 
