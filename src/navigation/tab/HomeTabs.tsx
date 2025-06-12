39622c60e534662f9a7445bf6689b192549b160d
ushasri2645 <ugudikandula@everest.engineering>
2025-06-12 16:27:00 +0530

    Add sync logic in home tab for global reflection.

--- src/navigation/tab/HomeTabs.tsx
+++ src/navigation/tab/HomeTabs.tsx
@@ -1,16 +1,15 @@
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {
   getFocusedRouteNameFromRoute,
-  useFocusEffect,
 } from '@react-navigation/native';
-import {useCallback, useEffect, useState} from 'react';
+import { useEffect, useState} from 'react';
 import {useTranslation} from 'react-i18next';
 import {Image, View} from 'react-native';
 import EncryptedStorage from 'react-native-encrypted-storage';
 import {useDispatch, useSelector} from 'react-redux';
 import {Badge} from '../../components/Badge/Badge.tsx';
 import {User} from '../../screens/Profile/Profile';
-import {getAllChats} from '../../services/GetAllChats.ts';
+import {getAllChats, getMissedChats} from '../../services/GetAllChats.ts';
 import {newSocket, socketConnection} from '../../socket/socket';
 import {
   setNewMessageCount,
@@ -22,6 +21,15 @@ import {HomeStacks} from '../stack/HomeStacks';
 import {ProfileStack} from '../stack/ProfileStacks';
 import {UnreadStacks} from '../stack/UnreadStacks.tsx';
 import {styles} from './HomeTabs.styles';
+import {insertToMessages} from '../../database/services/messageOperations.ts';
+import {
+  updateLastSyncedTime,
+  getLastSyncedTime,
+} from '../../database/services/userOperations.ts';
+import {generateMessageId} from '../../utils/messageId.ts';
+import {getTotalUnreadCount} from '../../database/services/chatOperations.ts';
+import {getDBInstance} from '../../database/connection/connection.ts';
+import {RootState} from '../../store/store.ts';
 
 const HomeTabIcon = ({focused}: {focused: boolean}) => {
   const {tabHome} = useImagesColors();
@@ -80,6 +88,8 @@ const ProfileTabIcon = ({focused}: {focused: boolean}) => {
 export const HomeTabs = () => {
   const [newMessageCount, setMessageCount] = useState(0);
   const dispatch = useDispatch();
+  const chatTrigger = useSelector((state: RootState) => state.chat);
+
   useEffect(() => {
     const handleNewMessage = (data: any) => {
       if (data.newMessage) {
@@ -106,24 +116,62 @@ export const HomeTabs = () => {
       newSocket.disconnect();
     };
   }, []);
-  useFocusEffect(
-    useCallback(() => {
-      const fetchChats = async () => {
-        const response = await getAllChats();
-        if (response.status === 200) {
-          const allChats = response.data.chats;
-          const unreadChats = allChats.filter(
-            (chat: {unreadCount: number}) => chat.unreadCount > 0,
-          );
 
-          const totalUnreadChats = unreadChats.length;
-          dispatch(setUnreadCount(totalUnreadChats));
+  useEffect(() => {
+    const fetchChats = async () => {
+      const response = await getAllChats();
+      if (response.status === 200) {
+        const allChats = response.data.chats;
+        const unreadChats = allChats.filter(
+          (chat: {unreadCount: number}) => chat.unreadCount > 0,
+        );
+
+        const totalUnreadChats = unreadChats.length;
+        dispatch(setUnreadCount(totalUnreadChats));
+      }
+    };
+    dispatch(setNewMessageCount(newMessageCount));
+    fetchChats();
+
+    const syncMessages = async () => {
+      const currentUser = await EncryptedStorage.getItem('user');
+      if (!currentUser) {
+        return;
+      }
+      const parsedUser: User = JSON.parse(currentUser);
+      let lastSyncedTime = await getLastSyncedTime(parsedUser.phoneNumber);
+      const response = await getMissedChats(lastSyncedTime);
+      if (response.status === 200 && response.data) {
+        const missedChats = response.data;
+        let latestTimestamp: string = new Date().toISOString();
+        for (const chat of missedChats) {
+          for (const message of chat.messages) {
+            if (message.senderPhoneNumber === parsedUser.phoneNumber) {
+              continue;
+            }
+            const messageToInsert = {
+              id: generateMessageId(
+                parsedUser.phoneNumber,
+                chat.senderPhoneNumber,
+                message.createdAt,
+              ),
+              senderPhoneNumber: chat.senderPhoneNumber as string,
+              receiverPhoneNumber: parsedUser.phoneNumber as string,
+              message: message.content as string,
+              status: message.status as 'sent' | 'delivered' | 'read',
+              timestamp: message.createdAt as string,
+            };
+            await insertToMessages(messageToInsert);
+          }
         }
-      };
-      dispatch(setNewMessageCount(newMessageCount));
-      fetchChats();
-    }, [dispatch, newMessageCount]),
-  );
+        await updateLastSyncedTime(latestTimestamp, parsedUser.phoneNumber);
+        const totalUnread = await getTotalUnreadCount(await getDBInstance());
+        dispatch(setUnreadCount(totalUnread));
+      }
+    };
+    syncMessages();
+  }, [dispatch, newMessageCount, chatTrigger]);
+
   const Tab = createBottomTabNavigator();
   const colors = useThemeColors();
   const {t} = useTranslation('home');
