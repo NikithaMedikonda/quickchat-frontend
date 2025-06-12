8c4084719775a201bcc581985b79fa69ddb5b900
ushasri2645 <ugudikandula@everest.engineering>
2025-06-12 16:27:58 +0530

    Write test to check creation of 2 other tables.

--- src/database/__tests__/schema.test.ts
+++ src/database/__tests__/schema.test.ts
@@ -1,5 +1,5 @@
-import { SQLiteDatabase } from 'react-native-sqlite-storage';
-import { createTables } from '../models/schema';
+import {SQLiteDatabase} from 'react-native-sqlite-storage';
+import {createTables} from '../models/schema';
 
 describe('Test for createTables function', () => {
   const mockExecuteSql = jest.fn();
@@ -18,21 +18,28 @@ describe('Test for createTables function', () => {
 
     await createTables(mockDb);
 
-    expect(mockExecuteSql).toHaveBeenCalledTimes(5);
+    expect(mockExecuteSql).toHaveBeenCalledTimes(7);
+    expect(mockExecuteSql).toHaveBeenCalledTimes(7);
     expect(mockExecuteSql.mock.calls[0][0]).toMatch(
       /CREATE TABLE IF NOT EXISTS Chats/i,
     );
     expect(mockExecuteSql.mock.calls[1][0]).toMatch(
-      /CREATE TABLE IF NOT EXISTS Messages/i,
+      /CREATE TABLE IF NOT EXISTS User/i,
     );
     expect(mockExecuteSql.mock.calls[2][0]).toMatch(
-      /CREATE TABLE\s+IF NOT EXISTS Queue/i,
+      /CREATE TABLE IF NOT EXISTS Messages/i,
     );
     expect(mockExecuteSql.mock.calls[3][0]).toMatch(
-      /CREATE TABLE IF NOT EXISTS Conversations/i,
+      /CREATE TABLE IF NOT EXISTS Queue/i,
     );
     expect(mockExecuteSql.mock.calls[4][0]).toMatch(
+      /CREATE TABLE IF NOT EXISTS Conversations/i,
+    );
+    expect(mockExecuteSql.mock.calls[5][0]).toMatch(
       /CREATE TABLE IF NOT EXISTS UserRestrictions/i,
+    );
+    expect(mockExecuteSql.mock.calls[6][0]).toMatch(
+      /CREATE TABLE IF NOT EXISTS LocalUsers/i,
     );
   });
 });
