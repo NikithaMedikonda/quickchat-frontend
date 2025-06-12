399af42b3c074531e9455155f0aaeb9976cd83cb
vinaysaichelupuri <vinaysai.chelupuri@everest.engineering>
2025-06-12 15:36:35 +0530

    Modified api.ts

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,5 +1,1 @@
-// export const API_URL = 'https://quickchat-backend-7pyd.onrender.com';
-
-import { Platform } from 'react-native';
-
-export const API_URL = Platform.OS === 'ios' ? `http://localhost:5050` : `http://192.168.0.5:5050`;
+export const API_URL = 'https://quickchat-backend-7pyd.onrender.com';
