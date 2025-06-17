2f0641e51fdc56c6b7122e81fd32166a79b30306
vinaysaichelupuri <vinaysai.chelupuri@everest.engineering>
2025-06-17 10:45:46 +0530

    Update ip for testing

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,4 +1,4 @@
 import { Platform } from "react-native";
 
 // export const API_URL = 'https://quickchat-backend-7pyd.onrender.com';
-export const API_URL = Platform.OS === 'ios' ? `http://localhost:5050` : `http://192.168.1.25:5050`;
+export const API_URL = Platform.OS === 'ios' ? `http://192.168.0.5:5050` : `http://192.168.0.5:5050`;
