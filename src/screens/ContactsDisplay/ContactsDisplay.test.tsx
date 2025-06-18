a9136bd26f24e3c557a40bb11ca1dac3d5f5bec4
ushasri2645 <ugudikandula@everest.engineering>
2025-06-18 11:42:54 +0530

    Modify test cases for contact display page

--- src/screens/ContactsDisplay/ContactsDisplay.test.tsx
+++ src/screens/ContactsDisplay/ContactsDisplay.test.tsx
@@ -1,3 +1,4 @@
+import {Alert, Platform} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import {
   cleanup,
@@ -6,7 +7,6 @@ import {
   screen,
   waitFor,
 } from '@testing-library/react-native';
-import {Platform} from 'react-native';
 import {Provider} from 'react-redux';
 import {numberNameIndex} from '../../helpers/nameNumberIndex';
 import {getContacts} from '../../services/GetContacts';
@@ -18,42 +18,34 @@ jest.setTimeout(10000);
 jest.mock('@react-navigation/native', () => ({
   useNavigation: jest.fn(),
 }));
-
 jest.mock('../../services/useDeviceCheck', () => ({
   useDeviceCheck: jest.fn(),
 }));
-
 jest.mock('react-native-contacts', () => ({
   getAll: jest.fn(),
   getContactsByPhoneNumber: jest.fn(),
 }));
-
 jest.mock('react-native-encrypted-storage', () => ({
   getItem: jest.fn(),
   setItem: jest.fn(),
   removeItem: jest.fn(),
 }));
-
 jest.mock('../../services/GetContacts', () => ({
   getContacts: jest.fn(),
 }));
-
 jest.mock('react-native-encrypted-storage', () => ({
   getItem: jest.fn(),
   setItem: jest.fn(),
   removeItem: jest.fn(),
 }));
-
 jest.mock('../../helpers/nameNumberIndex', () => ({
   numberNameIndex: jest.fn(),
 }));
-
 jest.mock('react-i18next', () => ({
   useTranslation: () => ({
     t: (str: string) => str,
   }),
 }));
-
 jest.mock('react-native-alert-notification', () => ({
   ALERT_TYPE: {
     DANGER: 'DANGER',
@@ -84,12 +76,10 @@ describe('Tests for ContactsDisplay Component', () => {
     };
     (useNavigation as jest.Mock).mockReturnValue(mockNavigation);
   });
-
   afterEach(() => {
     cleanup();
     jest.clearAllMocks();
   });
-
   it('should render contacts screen after successful fetch', async () => {
     const mockContacts = {
       registeredUsers: [
@@ -97,15 +87,12 @@ describe('Tests for ContactsDisplay Component', () => {
       ],
       unRegisteredUsers: ['+916303974914'],
     };
-
     (getContacts as jest.Mock).mockResolvedValue(mockContacts);
     (numberNameIndex as jest.Mock).mockResolvedValue({
       '+916303961097': 'Usha',
       '+916303974914': 'unknown',
     });
-
     renderComponent();
-
     await waitFor(() => {
       expect(screen.queryByTestId('loader')).toBeNull();
     });
@@ -113,7 +100,6 @@ describe('Tests for ContactsDisplay Component', () => {
     const inviteText = await screen.getByText('Invite to Quick Chat');
     const registeredUser = await screen.getByText('Usha');
     const unRegisteredUser = await screen.getByText('unknown');
-
     await waitFor(() => {
       expect(title).toBeTruthy();
       expect(inviteText).toBeTruthy();
@@ -121,7 +107,6 @@ describe('Tests for ContactsDisplay Component', () => {
       expect(unRegisteredUser).toBeTruthy();
     });
   });
-
   it('should handle pull to refresh functionality', async () => {
     const mockContacts = {
       registeredUsers: [
@@ -129,15 +114,12 @@ describe('Tests for ContactsDisplay Component', () => {
       ],
       unRegisteredUsers: ['+916303974914'],
     };
-
     (getContacts as jest.Mock).mockResolvedValue(mockContacts);
     (numberNameIndex as jest.Mock).mockResolvedValue({
       '+916303961097': 'Usha',
       '+916303974914': 'unknown',
     });
-
     renderComponent();
-
     await waitFor(
       () => {
         expect(screen.queryByTestId('loader')).toBeNull();
@@ -156,17 +138,14 @@ describe('Tests for ContactsDisplay Component', () => {
       expect(getContacts as jest.Mock).toHaveBeenCalledWith(true);
     });
   });
-
   it('should set the header options correctly', async () => {
     (getContacts as jest.Mock).mockResolvedValue({
       registeredUsers: [],
       unRegisteredUsers: ['+916303961098'],
     });
-
     await waitFor(() => {
       renderComponent();
     });
-
     await waitFor(() => {
       expect(mockNavigation.setOptions).toHaveBeenCalledWith(
         expect.objectContaining({
@@ -177,7 +156,6 @@ describe('Tests for ContactsDisplay Component', () => {
         }),
       );
     });
-
     const HeaderLeftComponent =
       mockNavigation.setOptions.mock.calls[0][0].headerLeft();
     render(<>{HeaderLeftComponent}</>);
@@ -190,7 +168,6 @@ describe('Tests for ContactsDisplay Component', () => {
       }
     });
   });
-
   it('should goback when tapped on goback arrow', async () => {
     renderComponent();
     await waitFor(() => {
@@ -200,6 +177,32 @@ describe('Tests for ContactsDisplay Component', () => {
       const image = screen.getByA11yHint('back-arrow-image');
       fireEvent.press(image);
       expect(mockNavigation.goBack).toHaveBeenCalled();
+    });
+  });
+  it('should set empty contacts if index is falsy', async () => {
+    const mockContacts = {
+      registeredUsers: [
+        {phoneNumber: '+916303961097', profilePicture: '', name: 'Usha'},
+      ],
+      unRegisteredUsers: ['+916303974914'],
+    };
+    (getContacts as jest.Mock).mockResolvedValue(mockContacts);
+    (numberNameIndex as jest.Mock).mockResolvedValue(null);
+    renderComponent();
+    await waitFor(() => {
+      expect(screen.queryByText('Usha')).toBeNull();
+    });
+  });
+  it('should show alert when fetchContacts throws error', async () => {
+    const mockError = new Error('Something went wrong');
+    (getContacts as jest.Mock).mockRejectedValue(mockError);
+    (numberNameIndex as jest.Mock).mockResolvedValue({});
+    const alertSpy = jest.spyOn(Alert, 'alert');
+    renderComponent();
+    await waitFor(() => {
+      expect(alertSpy).toHaveBeenCalledWith(
+        'Error while fetching the contacts: Something went wrong',
+      );
     });
   });
 });
