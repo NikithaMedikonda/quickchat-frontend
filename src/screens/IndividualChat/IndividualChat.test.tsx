12f3acb569083c567febe4140df49ca383c46859
durgamkeerthana <keerthana.durgam@everest.engineering>
2025-06-19 18:35:59 +0530

    Revert "Mock the missing receive deleted function"
    This reverts commit 5141dd14e850716a7ab89de7e54aa370f0605dc6.

--- src/screens/IndividualChat/IndividualChat.test.tsx
+++ src/screens/IndividualChat/IndividualChat.test.tsx
@@ -155,7 +155,6 @@ jest.mock('../../socket/socket', () => ({
   sendUpdatedMessages: jest.fn(),
   receiveReadUpdate: jest.fn(),
   receiveDeliveredStatus: jest.fn(),
-  receiveDeleted: jest.fn(),
   newSocket: {
     emit: jest.fn(),
     on: jest.fn(),
