980bd141ceda3c180ced1000f93b7dd34b8e25ba
NikithaMedikonda <nikitha.medikonda@everest.engineering>
2025-06-22 21:30:21 +0530

    Revert "Merge pull request #104 from NikithaMedikonda/fix-push-notifications-bugs"
    This reverts commit d9d32659f1f23350d36a22ffca7d8de03654f38d, reversing
    changes made to 9bc34399b8ebce5f6783b4e6b4af9b2b57f99ecf.

--- index.js
+++ index.js
@@ -8,24 +8,59 @@ import '@react-native-firebase/app';
 import { App } from './App';
 import { name as appName } from './app.json';
 import './src/i18n/i18n.config';
+import { numberNameIndex } from './src/helpers/nameNumberIndex';
+import { normalise } from './src/helpers/normalisePhoneNumber';
+import { DEFAULT_PROFILE_IMAGE } from './src/constants/defaultImage';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 messaging().setBackgroundMessageHandler(async (remoteMessage) => {
   await notifee.createChannel({
-    id: 'quickchatbg',
+    id: 'quickchat',
     name: 'QuickChat Notifications',
     importance: AndroidImportance.HIGH,
     sound : 'custom_notification',
   });
 
     const rawPhnoneNumber = remoteMessage.data?.senderPhoneNumber;
+    const rawPhoto = remoteMessage.data?.profilePicture;
     const senderPhoneNumber = typeof rawPhnoneNumber === 'string' ? rawPhnoneNumber : undefined;
+    const profilePicture = typeof rawPhoto === 'string' ? rawPhoto : undefined;
+    let contactName;
     if (senderPhoneNumber) {
+      try {
+        const nameIndex = await numberNameIndex();
+        const normalizedSender = normalise(senderPhoneNumber);
+        contactName = nameIndex?.[normalizedSender] || senderPhoneNumber;
+      } catch (error) {
+        contactName = senderPhoneNumber;
+      }
+    }
+
+  let messageCount = 1;
+  if (senderPhoneNumber) {
+    try {
+      const storedCount = await AsyncStorage.getItem(`msgCount_${senderPhoneNumber}`);
+      if (storedCount) {
+        messageCount = parseInt(storedCount, 10);
+        messageCount = isNaN(messageCount) ? 1 : messageCount;
+        messageCount += 1;
+      }
+    } catch (e) {
+      console.error('Error reading message count:', e);
+    }
+    await AsyncStorage.setItem(`msgCount_${senderPhoneNumber}`, messageCount.toString());
+  }
+
+
+    if (senderPhoneNumber || profilePicture) {
       await notifee.displayNotification({
         id: senderPhoneNumber,
-        title:'QuickChat',
-        body: 'New Message',
+        title:contactName,
+        body: `${messageCount} new message${messageCount > 1 ? 's' : ''}`,
         android: {
           channelId: 'quickchat',
           smallIcon:'ic_stat_notification',
+          largeIcon:profilePicture || DEFAULT_PROFILE_IMAGE,
+          circularLargeIcon:true,
           pressAction: {
             id: 'default',
           },
